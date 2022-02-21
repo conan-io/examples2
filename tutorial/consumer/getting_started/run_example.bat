30def9cf4249a71158dad6aeaa5f9fb630ee3c3c
czoido <mrgalleta@gmail.com>
2022-02-21 16:38:55 +0100

    wip

--- tutorial/consumer/getting_started/run_example.bat
+++ tutorial/consumer/getting_started/run_example.bat
@@ -3,6 +3,8 @@
 set BASEDIR=%~dp0
 PUSHD %BASEDIR%
 
+RMDIR /Q /S cmake-build
+
 conan install . --output-folder cmake-build --build=missing
 cmake . -G "Visual Studio 15 2017" -DCMAKE_TOOLCHAIN_FILE=cmake-build/conan_toolchain.cmake
 cmake --build . --config Release
