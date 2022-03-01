4da643da08d65c8b763df839f829220df9d3fcc3
czoido <mrgalleta@gmail.com>
2022-03-01 15:59:31 +0100

    fix

--- tutorial/consuming_packages/getting_started/conanfile_py/run_example.bat
+++ tutorial/consuming_packages/getting_started/conanfile_py/run_example.bat
@@ -6,6 +6,7 @@ PUSHD %BASEDIR%
 RMDIR /Q /S cmake-build
 
 conan install . --output-folder cmake-build --build=missing
+cd cmake-build
 cmake .. -G "Visual Studio 15 2017" -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake
 cmake --build . --config Release
 Release\compressor.exe
