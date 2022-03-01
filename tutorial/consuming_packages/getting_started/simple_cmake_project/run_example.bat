581d1738956322537b1de330a1c7393899da9e14
czoido <mrgalleta@gmail.com>
2022-03-01 16:53:43 +0100

    minor changes

--- tutorial/consuming_packages/getting_started/simple_cmake_project/run_example.bat
+++ tutorial/consuming_packages/getting_started/simple_cmake_project/run_example.bat
@@ -5,7 +5,7 @@ PUSHD %BASEDIR%
 
 RMDIR /Q /S cmake-build
 
-conan install . --output-folder build --build=missing
+conan install . --output-folder=build --build=missing
 cd build
 cmake .. -G "Visual Studio 15 2017" -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake
 cmake --build . --config Release
--- tutorial/consuming_packages/getting_started/simple_cmake_project/run_example.sh
+++ tutorial/consuming_packages/getting_started/simple_cmake_project/run_example.sh
@@ -8,7 +8,7 @@ pushd "$BASEDIR"
 
 rm -rf cmake-build-release
 
-conan install . --output-folder cmake-build-release --build=missing
+conan install . --output-folder=cmake-build-release --build=missing
 cd cmake-build-release
 cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake
 cmake --build .
