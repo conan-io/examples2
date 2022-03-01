4f6f99042308126b748f59d8490732fad7556950
czoido <mrgalleta@gmail.com>
2022-03-01 16:08:29 +0100

    update according to layout

--- tutorial/consuming_packages/getting_started/simple_cmake_project/run_example.bat
+++ tutorial/consuming_packages/getting_started/simple_cmake_project/run_example.bat
@@ -5,8 +5,8 @@ PUSHD %BASEDIR%
 
 RMDIR /Q /S cmake-build
 
-conan install . --output-folder cmake-build --build=missing
-cd cmake-build
+conan install . --output-folder build --build=missing
+cd build
 cmake .. -G "Visual Studio 15 2017" -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake
 cmake --build . --config Release
 Release\compressor.exe
