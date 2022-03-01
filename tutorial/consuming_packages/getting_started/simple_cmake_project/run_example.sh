f760793b5564c52db882a5cdd98977ba3c20460b
czoido <mrgalleta@gmail.com>
2022-03-01 15:39:30 +0100

    update commands

--- tutorial/consuming_packages/getting_started/simple_cmake_project/run_example.sh
+++ tutorial/consuming_packages/getting_started/simple_cmake_project/run_example.sh
@@ -9,6 +9,7 @@ pushd "$BASEDIR"
 rm -rf cmake-build-release
 
 conan install . --output-folder cmake-build-release --build=missing
-cmake . -DCMAKE_TOOLCHAIN_FILE=cmake-build-release/conan_toolchain.cmake
+cd cmake-build-release
+cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake
 cmake --build .
 ./compressor
