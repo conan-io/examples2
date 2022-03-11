bc8f521d871f8db2d0e55d1e034210e6dcf8b778
memsharded <james@conan.io>
2022-03-11 15:00:55 +0100

    wip

--- examples/ci/game/ai/src/ai.cpp
+++ examples/ci/game/ai/src/ai.cpp
@@ -5,9 +5,9 @@
 void ai(int intelligence){
     mathlib();
     #ifdef NDEBUG
-    std::cout << "ai/" << PKG_VERSION << ": Some Artificial Intelligence for enemies (Release)!\n";
+    std::cout << "ai/" << PKG_VERSION << ": Some Artificial Intelligence for aliens (Release)!\n";
     #else
-    std::cout << "ai/" << PKG_VERSION << ": Some Artificial Intelligence for enemies (Debug)!\n";
+    std::cout << "ai/" << PKG_VERSION << ": Some Artificial Intelligence for aliens (Debug)!\n";
     #endif
 
     std::cout << "ai/" << PKG_VERSION << ": Intelligence level="<< intelligence << "\n";
