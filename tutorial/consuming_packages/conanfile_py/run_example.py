778e67b5d9b95919d453632641baee12c76144b2
czoido <mrgalleta@gmail.com>
2022-05-05 16:06:45 +0200

    wip

--- tutorial/consuming_packages/conanfile_py/run_example.py
+++ tutorial/consuming_packages/conanfile_py/run_example.py
@@ -9,21 +9,17 @@ from examples_tools import chdir, run
 
 run("conan install . --build missing")
 
-run("dir")
-
 if platform.system() == "Windows":
     with chdir("build"):
         command = []
-        run("dir")
-        command.append("conanbuild.bat")
         command.append("cmake .. -G \"Visual Studio 15 2017\" -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake")
         command.append("cmake --build . --config Release")
-        command.append("deactivate_conanbuild.bat")
         run(" && ".join(command))
         cmd_out = run("Release/compressor.exe")
 else:
     with chdir("cmake-build-release"):
         command = []
+        # in the conanfile.py we only add CMake as tool_require in Linux
         command.append(". ./conanbuild.sh")
         command.append("cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release")
         command.append("cmake --build .")
