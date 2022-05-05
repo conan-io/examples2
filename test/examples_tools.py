43782d59f432eeb05fe9068a56613a44c0c5ff53
czoido <mrgalleta@gmail.com>
2022-05-05 15:41:41 +0200

    start simpler runs

---
+++ test/examples_tools.py
@@ -0,0 +1,34 @@
+import os
+import subprocess
+from contextlib import contextmanager
+
+
+@contextmanager
+def chdir(dir_path):
+    current = os.getcwd()
+    os.chdir(dir_path)
+    try:
+        yield
+    finally:
+        os.chdir(current)
+
+
+def run(cmd, error=False):
+    process = subprocess.Popen(
+        cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    out, err = process.communicate()
+    out = out.decode("utf-8")
+    err = err.decode("utf-8")
+    ret = process.returncode
+
+    output = err + out
+    print("Running: {}".format(cmd))
+    print("----- OUTPUT -------")
+    print(output)
+    print("----END OUTPUT------")
+    if ret != 0 and not error:
+        raise Exception("Failed cmd: {}\n{}".format(cmd, output))
+    if ret == 0 and error:
+        raise Exception(
+            "Cmd succeded (failure expected): {}\n{}".format(cmd, output))
+    return output
