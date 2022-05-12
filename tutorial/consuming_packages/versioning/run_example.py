cb30b8d104ec7f28b6839715d25ee706a181ad65
Carlos Zoido <mrgalleta@gmail.com>
2022-05-12 15:16:32 +0200

    Update tutorial/consuming_packages/versioning/run_example.py

--- tutorial/consuming_packages/versioning/run_example.py
+++ tutorial/consuming_packages/versioning/run_example.py
@@ -3,5 +3,5 @@ import platform
 from test.examples_tools import chdir, run
 
 
-run("conan install . --build=missing")
+cmd_out = run("conan install . --build=missing")
 assert "zlib/[~1.2]: zlib/1.2.12" in cmd_out
