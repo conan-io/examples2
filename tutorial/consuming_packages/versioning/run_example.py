c36a7ee09ba522450d7cd797495ac9420403dbd2
Carlos Zoido <mrgalleta@gmail.com>
2022-05-12 15:50:55 +0200

    remove zlib before example (#19)

--- tutorial/consuming_packages/versioning/run_example.py
+++ tutorial/consuming_packages/versioning/run_example.py
@@ -1,7 +1,6 @@
-import platform
-
 from test.examples_tools import chdir, run
 
 
+cmd_out = run('conan remove "zlib/*" -f')
 cmd_out = run("conan install . --build=missing")
 assert "zlib/[~1.2]: zlib/1.2.12" in cmd_out
