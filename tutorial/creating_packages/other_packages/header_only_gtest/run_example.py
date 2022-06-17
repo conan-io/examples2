6ba607fc0b5d915d30d295fa3e7957183a990080
Luis <lasote@gmail.com>
2022-06-17 13:16:39 +0200

    Use 14 and 17

--- tutorial/creating_packages/other_packages/header_only_gtest/run_example.py
+++ tutorial/creating_packages/other_packages/header_only_gtest/run_example.py
@@ -2,11 +2,11 @@ import platform
 
 from test.examples_tools import run
 
-cmd_out = run("conan create . -s compiler.cppstd=11 --build missing")
+cmd_out = run("conan create . -s compiler.cppstd=14 --build missing")
 assert "[  PASSED  ] 1 test." in cmd_out
 assert "sum/0.1: Package 'da39a3ee5e6b4b0d3255bfef95601890afd80709' built"
 
-cmd_out = run("conan create . -s compiler.cppstd=14")
+cmd_out = run("conan create . -s compiler.cppstd=17")
 assert "[  PASSED  ] 1 test." in cmd_out
 assert "sum/0.1: Package 'da39a3ee5e6b4b0d3255bfef95601890afd80709' built"
 
