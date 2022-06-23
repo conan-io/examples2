e54e8ac5667d88ebb7834bf965be00f07bd735fb
czoido <mrgalleta@gmail.com>
2022-06-23 17:50:04 +0200

    wip

--- tutorial/creating_packages/preparing_the_build/run_example.py
+++ tutorial/creating_packages/preparing_the_build/run_example.py
@@ -5,4 +5,10 @@ print("- Preparing the build with in the generate() method -")
 
 add_standard = "-s compiler.cppstd=11" if platform.system()!="Windows" else ""
 
-run(f"conan create . --build=missing {add_standard}")
+out = run(f"conan create . --build=missing {add_standard} -o with_fmt=True")
+
+assert "with color" in out
+
+out = run(f"conan create . --build=missing -o with_fmt=False")
+
+assert "without color" in out
