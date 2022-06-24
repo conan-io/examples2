a14326623c815ddd49e7f8edfbce179afc71108f
Francisco Ramirez de Anton <franchuti688@gmail.com>
2022-06-24 09:47:28 +0200

    Refactored

--- examples/extensions/commands/clean/cmd_clean.py
+++ examples/extensions/commands/clean/cmd_clean.py
@@ -21,7 +21,30 @@ def clean(conan_api: ConanAPIV2, parser, *args):
     args = parser.parse_args(*args)
 
     def confirmation(message):
+        """
+        Returns True or False if "--force" was passed or if user answers "yes" or "no" to the question message
+        """
         return args.force or ui.request_boolean(message)
+
+    def remove_all_recipe_revisions(rrevs):
+        """
+        Remove all the given recipes through ConanAPIV2.remove.recipe
+        """
+        # Removing all the earlier recipe revisions (and its packages)
+        if rrevs and confirmation(f"Remove all the {str(recipe)} recipe revisions but the last one?"):
+            for rrev in rrevs:
+                conan_api.remove.recipe(rrev, remote=remote)
+                out.writeln(f"  Removed RREV: {rrev.repr_notime()} [{output_remote}]", fg=removed_color)
+
+    def remove_all_package_revisions(prevs):
+        """
+        Remove all the given packages through ConanAPIV2.remove.package
+        """
+        # Removing all the earlier package revisions of the latest recipe revision
+        if prevs and confirmation(f"Remove all the {latest_rrev.repr_notime()} package revisions but the last one?"):
+            for prev in prevs:
+                conan_api.remove.package(prev, remote=remote)
+                out.writeln(f"  Removed PREV: {prev.repr_notime()} [{output_remote}]", fg=removed_color)
 
     out = ConanOutput()
     ui = UserInput(conan_api.config.get("core:non_interactive"))
@@ -31,22 +54,18 @@ def clean(conan_api: ConanAPIV2, parser, *args):
     recipes = conan_api.search.recipes("*/*", remote=remote)
     for recipe in recipes:
         out.writeln(f"{str(recipe)}", fg=recipe_color)
-        # All RREVS and latest one
+        # Get all the RREVs (recipe revisions)
         all_rrevs = conan_api.list.recipe_revisions(recipe, remote=remote)
+        # Save the latest RREV (it's the first one from the list)
         latest_rrev = all_rrevs.pop(0) if all_rrevs else None
+        # Remove the rest of RREVs and their packages
+        remove_all_recipe_revisions(all_rrevs)
         if latest_rrev:
-            # All PREVS and latest one (belonging to latest RREV)
+            # Get all the PREVs (package revisions) belonging to latest RREV
             all_prevs = conan_api.search.package_revisions(f"{latest_rrev.repr_notime()}:*#*", remote=remote)
+            # Save the latest PREV (it's the first one from the list)
             latest_prev = all_prevs.pop(0) if all_prevs else None
             if latest_prev:
                 out.writeln(f"  Keeping latest PREV: {latest_prev.repr_notime()} [{output_remote}]", fg=info_color)
-            # Removing all the earlier package revisions of the latest recipe revision
-            if all_prevs and confirmation(f"Remove all the {latest_rrev.repr_notime()} package revisions but the last one ?"):
-                for prev in all_prevs:
-                    conan_api.remove.package(prev, remote=remote)
-                    out.writeln(f"  Removed PREV: {prev.repr_notime()} [{output_remote}]", fg=removed_color)
-        # Removing all the earlier recipe revisions (and its packages)
-        if all_rrevs and confirmation(f"Remove all the {str(recipe)} recipe revisions but the last one ?"):
-            for rrev in all_rrevs:
-                conan_api.remove.recipe(rrev, remote=remote)
-                out.writeln(f"  Removed RREV: {rrev.repr_notime()} [{output_remote}]", fg=removed_color)
+            # Remove the rest of PREVs
+            remove_all_package_revisions(all_prevs)
