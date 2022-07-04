76242a26059fd0f41aecf5531dd58557a5096221
Luis <lasote@gmail.com>
2022-07-04 10:12:06 +0200

    Completed patching sources

---
+++ examples/tools/files/patches/conanfile_patch.py
@@ -0,0 +1,43 @@
+import os
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
+from conan.tools.files import get, replace_in_file, patch
+
+
+class helloRecipe(ConanFile):
+    name = "hello"
+    version = "1.0"
+
+    # Binary configuration
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+    exports_sources = "*.patch"
+
+    def source(self):
+        get(self, "https://github.com/conan-io/libhello/archive/refs/heads/main.zip", strip_root=True)
+        patch_file = os.path.join(self.export_sources_folder, "hello_patched.patch")
+        patch(self, patch_file=patch_file)
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["hello"]
--- examples/tools/files/patches/run_example.py
+++ examples/tools/files/patches/run_example.py
@@ -1,7 +1,4 @@
-import os
-import platform
-
-from test.examples_tools import run, tmp_dir
+from test.examples_tools import run
 
 # ############# Example ################
 print("- Patch the sources with different methods -")
@@ -18,3 +15,14 @@ assert "hello/1.0: Hello Static Friends!" in output
 output = run("conan create conanfile_replace_build.py -o shared=True")
 assert "hello/1.0: Hello Shared Friends!" in output
 
+print("Patching with patch tool")
+output = run("conan create conanfile_patch.py")
+assert "hello/1.0: Hello Patched World Release!" in output
+
+print("Patching with patch tool and conandata")
+output = run("conan create conanfile_patch_conandata.py")
+assert "hello/1.0: Hello Patched World Release!" in output
+
+print("Patching with apply_conandata_patches tool")
+output = run("conan create conanfile_apply_conandata.py")
+assert "hello/1.0: Hello Patched World Release!" in output
