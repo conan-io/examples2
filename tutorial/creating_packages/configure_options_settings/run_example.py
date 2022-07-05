1cf6db453e0f5c4d9e98bb0dbf3681fb15b253b3
czoido <mrgalleta@gmail.com>
2022-07-05 12:01:58 +0200

    wip

--- tutorial/creating_packages/configure_options_settings/run_example.py
+++ tutorial/creating_packages/configure_options_settings/run_example.py
@@ -5,6 +5,10 @@ print("- Configure settings and options in recipes -")
 
 add_standard = "-s compiler.cppstd=11" if platform.system()!="Windows" else ""
 
-out = run(f"conan create . --build=missing {add_standard} -s build_type=Release -tf=None")
+out = run(f"conan create . --build=missing {add_standard} -s build_type=Release -o shared=True -o fPIC=True -tf=None")
 
-out = run(f"conan create . --build=missing {add_standard} -s build_type=Debug -tf=None")
+assert "hello/1.0: Package" in out
+
+out = run(f"conan create . --build=missing {add_standard} -s build_type=Release -o shared=True -o fPIC=False -tf=None")
+
+assert "hello/1.0: Already installed!" in out
