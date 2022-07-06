182d80ad5de8c173aee6777dc79e8e900c2d148f
Luis <lasote@gmail.com>
2022-07-06 11:10:19 +0200

    Add package_id

--- tutorial/creating_packages/other_packages/tool_requires/run_example.py
+++ tutorial/creating_packages/other_packages/tool_requires/run_example.py
@@ -9,3 +9,10 @@ with chdir("consumer"):
     cmd_out = run('conan create .')
     assert re.search("Security Scanner: The path '.*' is secure!", cmd_out)
     assert "MY_VAR=23" in cmd_out
+
+
+with chdir("tool"):
+    cmd_out = run('conan create conanfile_package_id.py')
+    assert "Package '82339cc4d6db7990c1830d274cd12e7c91ab18a1' created" in cmd_out
+    cmd_out = run('conan create conanfile_package_id.py -s build_type=Debug')
+    assert "Package '82339cc4d6db7990c1830d274cd12e7c91ab18a1' created" in cmd_out
