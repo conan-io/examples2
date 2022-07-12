c41f0cea4223a7862533836aeaa1804034294100
czoido <mrgalleta@gmail.com>
2022-07-12 07:42:39 +0200

    add warning

--- tutorial/creating_packages/preparing_the_build/conanfile.py
+++ tutorial/creating_packages/preparing_the_build/conanfile.py
@@ -35,6 +35,8 @@ class helloRecipe(ConanFile):
     def source(self):
         git = Git(self)
         git.clone(url="https://github.com/conan-io/libhello.git", target=".")
+        # Please, be aware that using the head of the branch instead of an inmutable tag
+        # or commit is not a good practice in general
         git.checkout("optional_fmt")
 
     def requirements(self):
