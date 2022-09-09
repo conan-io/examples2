7f2a87aaddcc9394c2761a1809376784e432339a
czoido <mrgalleta@gmail.com>
2022-09-09 09:40:44 +0200

    wip

--- tutorial/creating_packages/build_method/run_example.py
+++ tutorial/creating_packages/build_method/run_example.py
@@ -7,10 +7,8 @@ add_standard = "-s compiler.cppstd=11" if platform.system()!="Windows" else ""
 
 out = run(f"conan create . {add_standard} --build=missing")
 
-assert "Built target test_hello" in out
 assert "Running 1 test from 1 test suite." in out
 
 out = run(f"conan create . {add_standard} --build=missing -c tools.build:skip_test=True")
 
-assert "Built target test_hello" not in out
 assert "Running 1 test from 1 test suite." not in out
