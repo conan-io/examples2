e656687896438dce36a61b67fabc58974dee980b
czoido <mrgalleta@gmail.com>
2022-09-09 08:02:33 +0200

    wip

--- tutorial/creating_packages/build_method/run_example.py
+++ tutorial/creating_packages/build_method/run_example.py
@@ -3,12 +3,12 @@ from test.examples_tools import run
 
 print("- Building and running tests in the build() method -")
 
-out = run(f"conan create . --build=missing")
+out = run(f"conan create . -s compiler.cppstd=gnu11 --build=missing")
 
 assert "Built target test_hello" in out
 assert "Running 1 test from 1 test suite." in out
 
-out = run(f"conan create . --build=missing -c tools.build:skip_test=True")
+out = run(f"conan create . -s compiler.cppstd=gnu11 --build=missing -c tools.build:skip_test=True")
 
 assert "Built target test_hello" not in out
 assert "Running 1 test from 1 test suite." not in out
