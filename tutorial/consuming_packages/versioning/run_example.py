2f6cbe34a12ce65b7a93723b6465e44c2c97bdc3
czoido <mrgalleta@gmail.com>
2022-12-02 15:11:34 +0100

    fix examples

--- tutorial/consuming_packages/versioning/run_example.py
+++ tutorial/consuming_packages/versioning/run_example.py
@@ -1,6 +1,6 @@
 from test.examples_tools import chdir, run
 
 
-cmd_out = run('conan remove "zlib/*" -f')
+cmd_out = run('conan remove "zlib/*" -c')
 cmd_out = run("conan install . --build=missing")
 assert "zlib/[~1.2]: zlib/1.2.12" in cmd_out
