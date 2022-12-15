6a1fed4485603c37f802d2a9aaa8299691d41b66
czoido <mrgalleta@gmail.com>
2022-12-15 15:35:29 +0100

    add ndk

--- examples/cross_build/android/ndk_basic/run_example.py
+++ examples/cross_build/android/ndk_basic/run_example.py
@@ -23,17 +23,14 @@ build_type=Debug
 tools.android:ndk_path={}
 """
 
-# FIXME: android-ndk missing in Mac M1
-if platform.system() == "Linux":
+ndk_path = {"Darwin": "/opt/homebrew/share/android-ndk", "Linux": "/opt/android-ndk-r23c"}.get(platform.system())
 
-    ndk_path = {"Darwin": "/usr/local/share/android-ndk", "Linux": "/opt/android-ndk-r23c"}.get(platform.system())
-
-    if ndk_path:
-        profile = profile.format(ndk_path)
-        with tmp_dir("tmp"):
-            with open("android", "w") as _f:
-                _f.write(profile)
-            run("conan new -d name=foo -d version=1.0 cmake_lib")
-            output = run("conan create . --profile ./android")
-            if platform.system() != "Linux":  # Linux with CMake 3.15 builds but print warns instead of this msg
-                assert "Targeting API '21' with architecture 'arm64', ABI 'arm64-v8a', and processor 'aarch64'" in output
+if ndk_path:
+    profile = profile.format(ndk_path)
+    with tmp_dir("tmp"):
+        with open("android", "w") as _f:
+            _f.write(profile)
+        run("conan new -d name=foo -d version=1.0 cmake_lib")
+        output = run("conan create . --profile ./android")
+        if platform.system() != "Linux":  # Linux with CMake 3.15 builds but print warns instead of this msg
+            assert "Targeting API '21' with architecture 'arm64', ABI 'arm64-v8a', and processor 'aarch64'" in output
