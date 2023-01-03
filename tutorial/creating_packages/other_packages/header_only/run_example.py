7593da54899c8a42d39f57a5b66841bf241e7a73
czoido <mrgalleta@gmail.com>
2023-01-03 09:21:41 +0100

    compat with beta7

--- tutorial/creating_packages/other_packages/header_only/run_example.py
+++ tutorial/creating_packages/other_packages/header_only/run_example.py
@@ -15,4 +15,6 @@ assert "1 + 3 = 4" in cmd_out
 cmd_out = run("conan list sum/0.1#:*")
 assert "Local Cache:" in cmd_out
 assert ": da39a3ee5e6b4b0d3255bfef95601890afd80709" in cmd_out
-assert "Empty package information" in cmd_out
+
+# FIXME: remove "No package info/revision was found" once beta8 is out
+assert any([pkg_info in cmd_out for pkg_info in ["Empty package information", "No package info/revision was found"]])
