cd3332af19d641e750c87fcd57b5ab3d1e558700
czoido <mrgalleta@gmail.com>
2023-01-17 14:28:18 +0100

    fix main

--- tutorial/creating_packages/other_packages/prebuilt_remote_binaries/run_example.py
+++ tutorial/creating_packages/other_packages/prebuilt_remote_binaries/run_example.py
@@ -10,4 +10,4 @@ run('conan create . -s os="Windows" -s arch="x86_64"')
 run('conan create . -s os="Windows" -s arch="armv8"')
 
 cmd_out = run('conan list hello/0.1#:*')
-assert cmd_out.count("settings:") == 6
+assert cmd_out.count("arch:") == 6
