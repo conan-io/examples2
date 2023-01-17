45961baf56f1c39ab23b2529741d974894717251
czoido <mrgalleta@gmail.com>
2023-01-17 09:57:41 +0100

    wip

--- tutorial/creating_packages/other_packages/header_only/run_example.py
+++ tutorial/creating_packages/other_packages/header_only/run_example.py
@@ -5,15 +5,15 @@ cmd_out = run("conan create .")
 assert "1 + 3 = 4" in cmd_out
 
 cmd_out = run("conan list sum/0.1#:*")
-assert "Local Cache:" in cmd_out
-assert ": da39a3ee5e6b4b0d3255bfef95601890afd80709" in cmd_out
+assert "Local Cache" in cmd_out
+assert "da39a3ee5e6b4b0d3255bfef95601890afd80709" in cmd_out
 
 
 cmd_out = run("conan create .")
 assert "1 + 3 = 4" in cmd_out
 
 cmd_out = run("conan list sum/0.1#:*")
-assert "Local Cache:" in cmd_out
-assert ": da39a3ee5e6b4b0d3255bfef95601890afd80709" in cmd_out
+assert "Local Cache" in cmd_out
+assert "da39a3ee5e6b4b0d3255bfef95601890afd80709" in cmd_out
 
-assert "Empty package information" in cmd_out
+assert "info" in cmd_out
