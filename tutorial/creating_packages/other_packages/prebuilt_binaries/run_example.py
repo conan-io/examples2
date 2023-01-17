f593db5637c710bd295fbaa06f6534d7b4eb77dd
Carlos Zoido <mrgalleta@gmail.com>
2023-01-17 12:08:39 +0100

    fix example (#59)

--- tutorial/creating_packages/other_packages/prebuilt_binaries/run_example.py
+++ tutorial/creating_packages/other_packages/prebuilt_binaries/run_example.py
@@ -11,5 +11,5 @@ run('conan export-pkg . -s os="Windows" -s arch="x86_64"')
 run('conan export-pkg . -s os="Windows" -s arch="armv8"')
 
 cmd_out = run('conan list hello/0.1#:*')
-assert cmd_out.count("settings:") == 6
+assert cmd_out.count("arch:") == 6
 
