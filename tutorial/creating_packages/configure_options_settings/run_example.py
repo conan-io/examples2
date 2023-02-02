572d60b9815cba54458f58a889f51c76e02691b3
czoido <mrgalleta@gmail.com>
2023-02-02 15:42:02 +0100

    remove tf

--- tutorial/creating_packages/configure_options_settings/run_example.py
+++ tutorial/creating_packages/configure_options_settings/run_example.py
@@ -7,7 +7,7 @@ add_standard = "-s compiler.cppstd=11" if platform.system()!="Windows" else ""
 
 ret_error = platform.system()=="Windows"
 
-out = run(f"conan create . --build=missing {add_standard} -s build_type=Release -o shared=True -o fPIC=True -tf=None", error=ret_error)
+out = run(f"conan create . --build=missing {add_standard} -s build_type=Release -o shared=True -o fPIC=True", error=ret_error)
 
 assertion = ("hello/1.0: Package" in out) if platform.system()!="Windows" else ("ERROR: option 'fPIC' doesn't exist" in out)
 
