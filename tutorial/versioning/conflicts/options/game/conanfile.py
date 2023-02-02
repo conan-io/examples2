422a308db8055bf5e8a69c1017546d8d666ee975
memsharded <james@conan.io>
2023-02-02 14:25:35 +0100

    wip

--- tutorial/versioning/conflicts/options/game/conanfile.py
+++ tutorial/versioning/conflicts/options/game/conanfile.py
@@ -4,7 +4,6 @@ from conan import ConanFile
 class Game(ConanFile):
     name = "game"
     version = "1.0"
-    default_options = {"matrix*:shared": True}
     
     def requirements(self):
         self.requires("engine/1.0")
