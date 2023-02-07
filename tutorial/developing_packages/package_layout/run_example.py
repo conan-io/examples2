213a1664f1b3260057e40ad2b40af963abaa53eb
czoido <mrgalleta@gmail.com>
2023-02-07 14:57:29 +0100

    simplify

--- tutorial/developing_packages/package_layout/run_example.py
+++ tutorial/developing_packages/package_layout/run_example.py
@@ -9,39 +9,26 @@ print("- Understanding the package layout -")
 
 # not in editable mode
 
+configure_preset = "default" if platform.system() == "Windows" else "release"
+
 with chdir("say"):
     run("conan create . -s build_type=Release")
 
 with chdir("hello"):
-    if platform.system() == "Windows":
-        run("conan install . -s build_type=Release")
-        run("cmake --preset default")
-        run("cmake --build --preset release")
-        cmd_out = run("build\Release\hello.exe")
-        assert "say/1.0: Hello World Release!" in cmd_out
-    else:
-        run("conan install . -s build_type=Release")
-        run("cmake --preset release")
-        run("cmake --build --preset release")
-        cmd_out = run("./build/Release/hello")
-        assert "say/1.0: Hello World Release!" in cmd_out
+    run("conan install . -s build_type=Release")
+    run(f"cmake --preset {configure_preset}")
+    run("cmake --build --preset release")
+    cmd_out = run("build\Release\hello.exe")
+    assert "say/1.0: Hello World Release!" in cmd_out
 
 with chdir("say"):
     replace(os.path.join("src", "say.cpp"), "Hello World", "Bye World")
     run("conan create . -s build_type=Release")
 
 with chdir("hello"):
-    if platform.system() == "Windows":        
-        run("cmake --build --preset release")
-        run("cmake --build --preset debug")
-        cmd_out = run("build\Release\hello.exe")
-        assert "say/1.0: Bye World Release!" in cmd_out
-        cmd_out = run("build\Debug\hello.exe")
-        assert "say/1.0: Bye World Debug!" in cmd_out
-    else:
-        run("cmake --build --preset release")
-        cmd_out = run("./build/Release/hello")
-        assert "say/1.0: Bye World Release!" in cmd_out
+    run("cmake --build --preset release")
+    cmd_out = run("build\Release\hello.exe")
+    assert "say/1.0: Bye World Release!" in cmd_out
 
 
 # use editable mode
@@ -49,54 +36,24 @@ with chdir("hello"):
 run("conan editable add say say/1.0")
 
 with chdir("say"):
-    if platform.system() == "Windows":
-        run("conan install . -s build_type=Release")
-        run("conan install . -s build_type=Debug")
-        run("cmake --preset default")
-        run("cmake --build --preset release")
-        run("cmake --build --preset debug")
-    else:
-        run("conan install . -s build_type=Release")
-        run("cmake --preset release")
-        run("cmake --build --preset release")
+    run("conan install . -s build_type=Release")
+    run(f"cmake --preset {configure_preset}")
+    run("cmake --build --preset release")
 
 with chdir("hello"):
-    if platform.system() == "Windows":
-        run("conan install . -s build_type=Release")
-        run("conan install . -s build_type=Debug")
-        run("cmake --preset default")
-        run("cmake --build --preset release")
-        run("cmake --build --preset debug")
-        cmd_out = run("build\Release\hello.exe")
-        assert "say/1.0: Hello World Release!" in cmd_out
-        cmd_out = run("build\Debug\hello.exe")
-        assert "say/1.0: Hello World Debug!" in cmd_out
-    else:
-        run("conan install . -s build_type=Release")
-        run("cmake --preset release")
-        run("cmake --build --preset release")
-        cmd_out = run("./build/Release/hello")
-        assert "say/1.0: Hello World Release!" in cmd_out
+    run("conan install . -s build_type=Release")
+    run("cmake --preset release")
+    run("cmake --build --preset release")
+    cmd_out = run("./build/Release/hello")
+    assert "say/1.0: Hello World Release!" in cmd_out
 
 with chdir("say"):
     replace(os.path.join("src", "say.cpp"), "Hello World", "Bye World")
-    if platform.system() == "Windows":        
-        run("cmake --build --preset release")
-        run("cmake --build --preset debug")
-    else:
-        run("cmake --build --preset release")
+    run("cmake --build --preset release")
 
 with chdir("hello"):
-    if platform.system() == "Windows":        
-        run("cmake --build --preset release")
-        run("cmake --build --preset debug")
-        cmd_out = run("build\Release\hello.exe")
-        assert "say/1.0: Bye World Release!" in cmd_out
-        cmd_out = run("build\Debug\hello.exe")
-        assert "say/1.0: Bye World Debug!" in cmd_out
-    else:
-        run("cmake --build --preset release")
-        cmd_out = run("./build/Release/hello")
-        assert "say/1.0: Bye World Release!" in cmd_out
+    run("cmake --build --preset release")
+    cmd_out = run("./build/Release/hello")
+    assert "say/1.0: Bye World Release!" in cmd_out
 
 run("conan editable remove say/1.0")
