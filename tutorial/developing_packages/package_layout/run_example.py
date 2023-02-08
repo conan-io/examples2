0958bb32e555a08331b6c4f7fb74294fac3be870
czoido <mrgalleta@gmail.com>
2023-02-08 16:35:35 +0100

    wip

--- tutorial/developing_packages/package_layout/run_example.py
+++ tutorial/developing_packages/package_layout/run_example.py
@@ -11,34 +11,22 @@ print("- Understanding the package layout -")
 
 configure_preset = "default" if platform.system() == "Windows" else "release"
 executable_binary = "build\Release\hello.exe" if platform.system() == "Windows" else "./build/Release/hello"
+lib_name = "say.lib" if platform.system() == "Windows" else "libsay.a"
 
 with chdir("say"):
     run("conan create . -s build_type=Release")
 
 with chdir("hello"):
     run("conan install . -s build_type=Release")
-    run(f"cmake --preset {configure_preset}")
+    cmd_out = run(f"cmake --preset {configure_preset} --log-level=VERBOSE")
+    folder = "\\p\\lib\\" if platform.system() == "Windows" else "/p/lib/"
+    assert f"{folder}{lib_name}" in cmd_out
     run("cmake --build --preset release")
     cmd_out = run(executable_binary)
     assert "say/1.0: Hello World Release!" in cmd_out
 
-with chdir("say"):
-    replace(os.path.join("src", "say.cpp"), "Hello World", "Bye World")
-    run("conan create . -s build_type=Release")
-
-with chdir("hello"):
-    # we have to install again as it's in another folder in the cache
-    run("conan install . -s build_type=Release")
-    run(f"cmake --preset {configure_preset}")
-    run("cmake --build --preset release")
-    cmd_out = run(executable_binary)
-    assert "say/1.0: Bye World Release!" in cmd_out
-
 
 # use editable mode
-
-# restore again the original say.cpp
-replace(os.path.join("src", "say.cpp"), "Bye World", "Hello World")
 
 run("conan editable add say say/1.0")
 
@@ -49,18 +37,11 @@ with chdir("say"):
 
 with chdir("hello"):
     run("conan install . -s build_type=Release")
-    run("cmake --preset release")
+    cmd_out = run(f"cmake --preset {configure_preset} --log-level=VERBOSE")
+    folder = "say\\build\\Release\\" if platform.system() == "Windows" else "say/build/Release/"
+    assert f"{folder}{lib_name}" in cmd_out
     run("cmake --build --preset release")
     cmd_out = run(executable_binary)
     assert "say/1.0: Hello World Release!" in cmd_out
-
-with chdir("say"):
-    replace(os.path.join("src", "say.cpp"), "Hello World", "Bye World")
-    run("cmake --build --preset release")
-
-with chdir("hello"):
-    run("cmake --build --preset release")
-    cmd_out = run(executable_binary)
-    assert "say/1.0: Bye World Release!" in cmd_out
 
 run("conan editable remove say/1.0")
