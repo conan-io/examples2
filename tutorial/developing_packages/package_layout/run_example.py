c8169495abb7dc99b3004cdc766f198ae88a3516
czoido <mrgalleta@gmail.com>
2023-02-09 10:04:53 +0100

    wip

--- tutorial/developing_packages/package_layout/run_example.py
+++ tutorial/developing_packages/package_layout/run_example.py
@@ -5,43 +5,46 @@ import shutil
 
 from test.examples_tools import run, chdir, replace
 
+# TODO: for the moment the layout in the tutorial is only explained
+# for the single config case
+if platform.system() != "Windows":
 
-print("- Understanding the package layout -")
+    print("- Understanding the package layout -")
 
-# not in editable mode
+    # not in editable mode
 
-configure_preset = "default" if platform.system() == "Windows" else "release"
-executable_binary = "build\Release\hello.exe" if platform.system() == "Windows" else "./build/Release/hello"
-lib_name = "say.lib" if platform.system() == "Windows" else "libsay.a"
+    configure_preset = "default" if platform.system() == "Windows" else "release"
+    executable_binary = "build\Release\hello.exe" if platform.system() == "Windows" else "./build/Release/hello"
+    lib_name = "say.lib" if platform.system() == "Windows" else "libsay.a"
 
-with chdir("say"):
-    run("conan create . -s build_type=Release")
+    with chdir("say"):
+        run("conan create . -s build_type=Release")
 
-with chdir("hello"):
-    run("conan install . -s build_type=Release")
-    cmd_out = run(f"cmake --preset {configure_preset} --log-level=VERBOSE")
-    assert f"/p/lib/{lib_name}" in cmd_out
-    run("cmake --build --preset release")
-    cmd_out = run(executable_binary)
-    assert "say/1.0: Hello World Release!" in cmd_out
+    with chdir("hello"):
+        run("conan install . -s build_type=Release")
+        cmd_out = run(f"cmake --preset {configure_preset} --log-level=VERBOSE")
+        assert f"/p/lib/{lib_name}" in cmd_out
+        run("cmake --build --preset release")
+        cmd_out = run(executable_binary)
+        assert "say/1.0: Hello World Release!" in cmd_out
 
 
-# use editable mode
+    # use editable mode
 
-run("conan editable add say say/1.0")
+    run("conan editable add say say/1.0")
 
-with chdir("say"):
-    run("conan install . -s build_type=Release")
-    run(f"cmake --preset {configure_preset}")
-    run("cmake --build --preset release")
+    with chdir("say"):
+        run("conan install . -s build_type=Release")
+        run(f"cmake --preset {configure_preset}")
+        run("cmake --build --preset release")
 
-with chdir("hello"):
-    shutil.rmtree("./build")
-    run("conan install . -s build_type=Release")
-    cmd_out = run(f"cmake --preset {configure_preset} --log-level=VERBOSE")
-    assert f"say/build/Release/{lib_name}" in cmd_out
-    run("cmake --build --preset release")
-    cmd_out = run(executable_binary)
-    assert "say/1.0: Hello World Release!" in cmd_out
+    with chdir("hello"):
+        shutil.rmtree("./build")
+        run("conan install . -s build_type=Release")
+        cmd_out = run(f"cmake --preset {configure_preset} --log-level=VERBOSE")
+        assert f"say/build/Release/{lib_name}" in cmd_out
+        run("cmake --build --preset release")
+        cmd_out = run(executable_binary)
+        assert "say/1.0: Hello World Release!" in cmd_out
 
-run("conan editable remove say/1.0")
+    run("conan editable remove say/1.0")
