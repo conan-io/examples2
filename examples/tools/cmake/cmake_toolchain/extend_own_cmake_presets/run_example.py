abdb62c480450c95c8575f6f22f615a0c79dc5bd
czoido <mrgalleta@gmail.com>
2023-02-17 16:14:58 +0100

    fix

--- examples/tools/cmake/cmake_toolchain/extend_own_cmake_presets/run_example.py
+++ examples/tools/cmake/cmake_toolchain/extend_own_cmake_presets/run_example.py
@@ -21,8 +21,13 @@ else:
     run(f"cmake --preset debug")
     run(f"cmake --build --preset debug")
 
-output = run(str(os.path.join("build", "Release", "foo")))
-assert "Hello World Release!" in output
-
-output = run(str(os.path.join("build", "Debug", "foo")))
-assert "Hello World Debug!" in output
+if platform.system() == "Windows":        
+    output = run("build\\Release\\foo.exe")
+    assert "Hello World Release!" in output
+    output = run("build\\Debug\\foo.exe")
+    assert "Hello World Debug!" in output
+else:
+    output = run(str(os.path.join("build", "Release", "foo")))
+    assert "Hello World Release!" in output
+    output = run(str(os.path.join("build", "Debug", "foo")))
+    assert "Hello World Debug!" in output
