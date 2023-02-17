08d7f3cf88346785f44e04298b62c75fd3fa77b7
czoido <mrgalleta@gmail.com>
2023-02-17 15:52:29 +0100

    wip

---
+++ examples/tools/cmake/cmake_toolchain/extend_own_cmake_presets/conanfile.py
@@ -0,0 +1,42 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
+
+
+class fooRecipe(ConanFile):
+    name = "foo"
+    version = "1.0"
+    package_type = "application"
+
+    # Optional metadata
+    license = "<Put the package license here>"
+    author = "<Put your name here> <And your email here>"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "<Description of foo package here>"
+    topics = ("<Put some tag here>", "<here>", "<and here>")
+
+    # Binary configuration
+    settings = "os", "compiler", "build_type", "arch"
+
+    # Sources are located in the same place as this recipe, copy them to the recipe
+    exports_sources = "CMakeLists.txt", "src/*"
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+        tc.user_presets_path = 'ConanPresets.json'
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    
--- examples/tools/cmake/cmake_toolchain/extend_own_cmake_presets/run_example.py
+++ examples/tools/cmake/cmake_toolchain/extend_own_cmake_presets/run_example.py
@@ -8,63 +8,6 @@ from conan import conan_version
 
 print("- CMakeToolchain: Extending your CMakePresets with Conan generated ones -")
 
-run("conan new -d name=foo -d version=1.0 cmake_exe")
-
-replace("conanfile.py", "tc.generate()", "tc.user_presets_path = 'ConanPresets.json'\n        tc.generate()")
-
-cmake_presets = textwrap.dedent("""
-    {
-    "version": 4,
-    "include": ["./ConanPresets.json"],
-    "configurePresets": [
-        {
-            "name": "default",
-            "displayName": "multi config",
-            "inherits": "conan-default"
-        },
-        {
-            "name": "release",
-            "displayName": "release single config",
-            "inherits": "conan-release"
-        },
-        {
-            "name": "debug",
-            "displayName": "debug single config",
-            "inherits": "conan-debug"
-        }
-    ],
-    "buildPresets": [
-        {
-            "name": "multi-release",
-            "configurePreset": "default",
-            "configuration": "Release",
-            "inherits": "conan-release"
-        },
-        {
-            "name": "multi-debug",
-            "configurePreset": "default",
-            "configuration": "Debug",
-            "inherits": "conan-debug"
-        },
-        {
-            "name": "release",
-            "configurePreset": "release",
-            "configuration": "Release",
-            "inherits": "conan-release"
-        },
-        {
-            "name": "debug",
-            "configurePreset": "debug",
-            "configuration": "Debug",
-            "inherits": "conan-debug"
-        }
-    ]
-    }""")
-
-with open("CMakePresets.json", "w") as f:
-    f.write(cmake_presets)
-
-
 run("conan install .")
 run("conan install . -s build_type=Debug")
 
