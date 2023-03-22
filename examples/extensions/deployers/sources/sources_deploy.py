5e510413f10bcc30dbaccb82edaf495129120fe4
Rubén Rincón Blanco <rubenrb@jfrog.com>
2023-03-22 22:27:36 +0100

    Add exception for None sources_folder

--- examples/extensions/deployers/sources/sources_deploy.py
+++ examples/extensions/deployers/sources/sources_deploy.py
@@ -1,7 +1,12 @@
+from conan.errors import ConanException
 from conan.tools.files import copy
 import os
 
 
 def deploy(graph, output_folder):
     for name, dep in graph.root.conanfile.dependencies.items():
+        if dep.folders is None or dep.folders.source_folder is None:
+            raise ConanException(f"Sources missing for {name} dependency.\n"
+                                 "This deployer needs the sources of every dependency present to work, either building from source, "
+                                 "or by using the 'tools.build:download_source' conf.")
         copy(graph.root.conanfile, "*", dep.folders.source_folder, os.path.join(output_folder, "dependencies_sources", str(dep)))
