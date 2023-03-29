c4980f1a40cc504f7683eb6ced54f86207ba2ba0
czoido <mrgalleta@gmail.com>
2023-03-29 07:46:54 +0200

    wip

--- tutorial/consuming_packages/conanfile_py/conanfile.py
+++ tutorial/consuming_packages/conanfile_py/conanfile.py
@@ -1,29 +1,12 @@
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
 
 
 class CompressorRecipe(ConanFile):
-    # Binary configuration
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeToolchain", "CMakeDeps"
 
     def requirements(self):
         self.requires("zlib/1.2.11")
-        # Add base64 dependency only for Windows
-        if self.settings.os == "Windows":
-            self.requires("base64/0.4.0")
 
     def build_requirements(self):
-        if self.settings.os != "Windows":  # we need cmake 3.22.6 in other platforms
-            self.tool_requires("cmake/3.22.6")
-
-    def layout(self):
-        # We make the assumption that if the compiler is msvc the
-        # CMake generator is multi-config
-        if self.settings.get_safe("compiler") == "msvc":
-            multi = True
-        else:
-            multi = False          
-
-        self.folders.build = "build" if multi else f"build/{str(self.settings.build_type)}"
-        self.folders.generators = "build"
+        self.tool_requires("cmake/3.22.6")
--- tutorial/consuming_packages/conanfile_py/run_example.py
+++ tutorial/consuming_packages/conanfile_py/run_example.py
@@ -1,29 +1,49 @@
 import platform
+import os
 
 from test.examples_tools import chdir, run
 
+def run_example():
 
-# following the cmake-layout declared in the conanfile.py
-# it will install the conan generated files in 'build' folder
+    run("conan install . --output-folder=build --build missing")
 
-run("conan install . --build missing")
+    if platform.system() == "Windows":
+        with chdir("build"):
+            command = []
+            command.append("cmake .. -G \"Visual Studio 15 2017\" -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake")
+            command.append("cmake --build . --config Release")
+            run(" && ".join(command))
+            cmd_out = run("Release\\compressor.exe")
+    else:
+        with chdir("build"):
+            command = []
+            # in the conanfile.py we only add CMake as tool_require in Linux
+            command.append(". ./conanbuild.sh")
+            command.append("cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release")
+            command.append("cmake --build .")
+            command.append(". ./deactivate_conanbuild.sh")
+            run(" && ".join(command))
+            cmd_out = run("./compressor")
 
-if platform.system() == "Windows":
-    with chdir("build"):
-        command = []
-        command.append("cmake .. -G \"Visual Studio 15 2017\" -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake")
-        command.append("cmake --build . --config Release")
-        run(" && ".join(command))
-        cmd_out = run("Release\\compressor.exe")
-else:
-    with chdir("build"):
-        command = []
-        # in the conanfile.py we only add CMake as tool_require in Linux
-        command.append(". ./conanbuild.sh")
-        command.append("cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release")
-        command.append("cmake --build .")
-        command.append(". ./deactivate_conanbuild.sh")
-        run(" && ".join(command))
-        cmd_out = run("./compressor")
+    assert "ZLIB VERSION: 1.2.11" in cmd_out
 
-assert "ZLIB VERSION: 1.2.11" in cmd_out
+
+print("- Understanding the flexibility of using conanfile.py vs conanfile.txt -")
+
+# first run the basic example without layout and conditionals 
+
+run_example()
+
+# switch the conanfile's and code and run the more advanced example
+
+os.rename("conanfile.py", "basic_conanfile.py")
+os.rename(os.path.join("src", "main.c"), os.path.join("src", "basic_main.c"))
+os.rename("complete_conanfile.py", "conanfile.py")
+os.rename(os.path.join("src", "complete_main.c"), os.path.join("src", "main.c"))
+
+run_example()
+
+os.rename("conanfile.py", "complete_conanfile.py")
+os.rename(os.path.join("src", "main.c"), os.path.join("src", "complete_main.c"))
+os.rename("basic_conanfile.py", "conanfile.py")
+os.rename(os.path.join("src", "basic_main.c"), os.path.join("src", "main.c"))
