551d449e04f7442a16946e3c6d458c8b55ac566b
czoido <mrgalleta@gmail.com>
2023-04-20 12:02:43 +0200

    wip

--- examples/libraries/tensorflow-lite/pose-estimation/run_example.py
+++ examples/libraries/tensorflow-lite/pose-estimation/run_example.py
@@ -1,3 +1,4 @@
+import platform
 from test.examples_tools import run
 
 print("Pose estimation example with Tensorflow Lite and OpenCV")
@@ -6,6 +7,33 @@ run("conan install . -o opencv/\*:with_ffmpeg=False "
     "-o opencv/\*:with_gtk=False -c tools.system.package_manager:mode=install "
     "-c tools.system.package_manager:sudo=True")
 
-run("cmake --preset conan-release")
-run("cmake --build --preset conan-release")
-run("build/Release/pose-estimation --no-windows")
+# with presets
+
+if platform.system() == "Windows":
+    run("cmake --preset conan-release")
+    run("cmake --build --preset conan-release")
+    run("build/Release/pose-estimation --no-windows")
+else:
+    run("cmake --preset conan-default")
+    run("cmake --build --preset conan-release")
+    run("build\Release\pose-estimation.exe --no-windows")
+
+if platform.system() == "Windows":
+    run("rd /s /q build")
+else:
+    run("rm -rf build")
+
+run("conan install . -o opencv/\*:with_ffmpeg=False "
+    "-o opencv/\*:with_gtk=False -c tools.system.package_manager:mode=install "
+    "-c tools.system.package_manager:sudo=True")
+
+# calling CMake directly
+
+if platform.system() == "Windows":
+    run("cmake . -G \"Unix Makefiles\" -DCMAKE_TOOLCHAIN_FILE=build/Release/generators/conan_toolchain.cmake -DCMAKE_POLICY_DEFAULT_CMP0091=NEW -DCMAKE_BUILD_TYPE=Release")
+    run("cmake --build .")
+    run("./pose-estimation --no-windows")
+else:
+    run("cmake .. -G \"Visual Studio 15 2017\" -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_POLICY_DEFAULT_CMP0091=NEW'")
+    run("cmake --build . --config Release")
+    run("pose-estimation.exe --no-windows")
