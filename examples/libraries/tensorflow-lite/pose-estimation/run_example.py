2fa36ee1b08b5df7d69d35c89eab178472901e10
czoido <mrgalleta@gmail.com>
2023-04-20 13:03:33 +0200

    wip

--- examples/libraries/tensorflow-lite/pose-estimation/run_example.py
+++ examples/libraries/tensorflow-lite/pose-estimation/run_example.py
@@ -30,7 +30,7 @@ run("conan install . -o opencv/*:with_ffmpeg=False "
 # calling CMake directly
 
 if platform.system() == "Windows":
-    run("cmake .. -G \"Visual Studio 15 2017\" -DCMAKE_TOOLCHAIN_FILE=./build/generators/conan_toolchain.cmake -DCMAKE_POLICY_DEFAULT_CMP0091=NEW'")
+    run("cmake .. -G \"Visual Studio 17 2022\" -DCMAKE_TOOLCHAIN_FILE=./build/generators/conan_toolchain.cmake -DCMAKE_POLICY_DEFAULT_CMP0091=NEW'")
     run("cmake --build . --config Release")
     run("dir")
     run("pose-estimation.exe --no-windows")
