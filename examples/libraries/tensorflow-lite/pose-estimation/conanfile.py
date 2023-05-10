a91da469c32a3f26942639913b14be2073be853a
czoido <mrgalleta@gmail.com>
2023-05-10 06:53:59 +0200

    remove overrides

--- examples/libraries/tensorflow-lite/pose-estimation/conanfile.py
+++ examples/libraries/tensorflow-lite/pose-estimation/conanfile.py
@@ -9,8 +9,6 @@ class PoseEstimationRecipe(ConanFile):
     def requirements(self):
         self.requires("tensorflow-lite/2.10.0")
         self.requires("opencv/4.5.5")
-        self.requires("libwebp/1.3.0", override=True)
-        self.requires("eigen/3.4.0", override=True)
 
     def layout(self):
         cmake_layout(self)
