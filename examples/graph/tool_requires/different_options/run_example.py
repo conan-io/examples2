f3b28d110eaaf166a9b963fd2146724cac4ef660
czoido <mrgalleta@gmail.com>
2023-05-18 09:01:27 +0200

    check conan version

--- examples/graph/tool_requires/different_options/run_example.py
+++ examples/graph/tool_requires/different_options/run_example.py
@@ -1,11 +1,11 @@
-import os
+from conan import conan_version
+from test.examples_tools import run
 
-from test.examples_tools import run, tmp_dir
 
+if conan_version >= "2.0.5":
+    run("conan create gcc -o myoption=1")
+    run("conan create gcc -o myoption=2")
 
-run("conan create gcc -o myoption=1")
-run("conan create gcc -o myoption=2")
-
-output = run("conan create wine")
-assert "MYGCC=1!!" in output
-assert "MYGCC=2!!" in output
+    output = run("conan create wine")
+    assert "MYGCC=1!!" in output
+    assert "MYGCC=2!!" in output
