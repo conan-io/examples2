d31431478e85df8130a2d7aa39ce14b9ce72562f
Uilian Ries <uilianries@gmail.com>
2023-05-23 12:32:37 +0200

    install python protobuf
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- examples/libraries/protobuf/serialization/run_example.bat
+++ examples/libraries/protobuf/serialization/run_example.bat
@@ -4,6 +4,8 @@ RMDIR /Q /S build
 MKDIR build
 PUSHD build
 
+pip install -r ../requirements.txt
+
 conan install .. -pr:h=default -pr:b=default --build=missing
 cmake .. -DCMAKE_TOOLCHAIN_FILE=Release/generators/conan_toolchain.cmake
 cmake --build . --config Release
--- examples/libraries/protobuf/serialization/run_example.sh
+++ examples/libraries/protobuf/serialization/run_example.sh
@@ -3,9 +3,11 @@
 set -e
 set -x
 
-rm -rf build
+rm -rf build/ CMakeUserPresets.json
 mkdir build
 pushd build
+
+pip install -r ../requirements.txt
 
 conan install .. -pr:h=default -pr:b=default --build=missing
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=Release/generators/conan_toolchain.cmake
