ef24f2a8f1c8a52353f91c656e913ac0f40f2062
Uilian Ries <uilianries@gmail.com>
2023-05-23 11:18:56 +0200

    do not ask for CMAKE_GENERATOR
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- examples/libraries/protobuf/serialization/build.bat
+++ examples/libraries/protobuf/serialization/build.bat
@@ -1,18 +1,14 @@
-if "%CMAKE_GENERATOR%"=="" (
-    ECHO CMAKE_GENERATOR environment variable not defined. Please define the CMake generator in the CMAKE_GENERATOR environment variable.
-) else (
-    @ECHO ON
+@ECHO ON
 
-    RMDIR /Q /S build
-    MKDIR build
-    PUSHD build
+RMDIR /Q /S build
+MKDIR build
+PUSHD build
 
-    conan install .. -pr:h=default -pr:b=default --build=missing
-    cmake .. -DCMAKE_TOOLCHAIN_FILE=Release/generators/conan_toolchain.cmake
-    cmake --build . --config Release
+conan install .. -pr:h=default -pr:b=default --build=missing
+cmake .. -DCMAKE_TOOLCHAIN_FILE=Release/generators/conan_toolchain.cmake
+cmake --build . --config Release
 
-    sensor.exe
+sensor.exe
 
-    SET PYTHONPATH="%CD%"
-    python ../main.py
-)
+SET PYTHONPATH="%CD%"
+python ../main.py
