a06d375786c07e17153ce224e85e510f7ba7da20
Christopher McArthur <christopherm@jfrog.com>
2023-06-29 11:00:01 -0700

    improved packaging of image displayed and test unpackaging

--- examples/libraries/sdl2/conanfile.py
+++ examples/libraries/sdl2/conanfile.py
@@ -1,24 +1,21 @@
 from conan import ConanFile
-from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
+from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.files import copy
 
 
-class sdl_exampleRecipe(ConanFile):
+class SdlExampleRecipe(ConanFile):
     name = "sdl-example"
     version = "1.0"
     package_type = "application"
-
-    # Optional metadata
-    license = "<Put the package license here>"
-    author = "<Put your name here> <And your email here>"
-    url = "<Package recipe repository url here, for issues about the package>"
-    description = "<Description of sdl-example package here>"
-    topics = ("<Put some tag here>", "<here>", "<and here>")
 
     # Binary configuration
     settings = "os", "compiler", "build_type", "arch"
 
     # Sources are located in the same place as this recipe, copy them to the recipe
-    exports_sources = "CMakeLists.txt", "src/*"
+    exports_sources = "CMakeLists.txt", "src/*", "conan-logo.png"
+
+    # Required Generators to integrate with CMake build scripts
+    generators = "CMakeDeps", "CMakeToolchain"
 
     def layout(self):
         cmake_layout(self)
@@ -28,18 +25,13 @@ class sdl_exampleRecipe(ConanFile):
         self.requires("sdl/2.26.1") # 2.26.5 is a version conflict with sdl_image
         self.requires("sdl_image/2.0.5")
 
-    def generate(self):
-        deps = CMakeDeps(self)
-        deps.generate()
-        tc = CMakeToolchain(self)
-        tc.generate()
-
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
     def package(self):
+        copy(self, "conan-logo.png", self.export_sources_folder, self.package_folder)
         cmake = CMake(self)
         cmake.install()
         
--- examples/libraries/sdl2/test_package/conanfile.py
+++ examples/libraries/sdl2/test_package/conanfile.py
@@ -1,13 +1,16 @@
-import os
 from conan import ConanFile
 from conan.tools.build import can_run
+from conan.tools.files import copy
 
 
-class sdl_exampleTestConan(ConanFile):
+class SdlExampleTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
 
     def requirements(self):
         self.requires(self.tested_reference_str)
+
+    def generate(self):
+        copy(self, "*.png", self.dependencies[self.tested_reference_str].package_folder, self.build_folder)
 
     def test(self):
         if can_run(self):
