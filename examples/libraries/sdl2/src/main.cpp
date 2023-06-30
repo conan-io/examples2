3d64b49633e07fb40e001a65f8b07b0978172d88
Christopher McArthur <christopherm@jfrog.com>
2023-06-30 13:14:20 -0700

    fix whitespace

--- examples/libraries/sdl2/src/main.cpp
+++ examples/libraries/sdl2/src/main.cpp
@@ -32,10 +32,10 @@ void render_text(
 int main(int argc, char *argv[])
 {
 
-	// returns zero on success else non-zero
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-		printf("error initializing SDL: %s\n", SDL_GetError());
-	}
+    // returns zero on success else non-zero
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        printf("error initializing SDL: %s\n", SDL_GetError());
+    }
 
     /* Init TTF. */
     TTF_Init();
@@ -45,142 +45,142 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-	SDL_Window* win = SDL_CreateWindow("GAME", // creates a window
-									SDL_WINDOWPOS_CENTERED,
-									SDL_WINDOWPOS_CENTERED,
-									1000, 1000, 0);
+    SDL_Window* win = SDL_CreateWindow("GAME", // creates a window
+                                    SDL_WINDOWPOS_CENTERED,
+                                    SDL_WINDOWPOS_CENTERED,
+                                    1000, 1000, 0);
 
-	// triggers the program that controls
-	// your graphics hardware and sets flags
-	Uint32 render_flags = SDL_RENDERER_ACCELERATED;
+    // triggers the program that controls
+    // your graphics hardware and sets flags
+    Uint32 render_flags = SDL_RENDERER_ACCELERATED;
 
-	// creates a renderer to render our images
-	SDL_Renderer* rend = SDL_CreateRenderer(win, -1, render_flags);
+    // creates a renderer to render our images
+    SDL_Renderer* rend = SDL_CreateRenderer(win, -1, render_flags);
 
-	// creates a surface to load an image into the main memory
-	SDL_Surface* surface;
+    // creates a surface to load an image into the main memory
+    SDL_Surface* surface;
 
-	// please provide a path for your image
-	surface = IMG_Load("conan-logo.png");
+    // please provide a path for your image
+    surface = IMG_Load("conan-logo.png");
 
-	// loads image to our graphics hardware memory.
-	SDL_Texture* tex = SDL_CreateTextureFromSurface(rend, surface);
+    // loads image to our graphics hardware memory.
+    SDL_Texture* tex = SDL_CreateTextureFromSurface(rend, surface);
 
-	// clears main-memory
-	SDL_FreeSurface(surface);
+    // clears main-memory
+    SDL_FreeSurface(surface);
 
-	// let us control our image position
-	// so that we can move it with our keyboard.
-	SDL_Rect dest;
+    // let us control our image position
+    // so that we can move it with our keyboard.
+    SDL_Rect dest;
 
-	SDL_Rect text_rect;
+    SDL_Rect text_rect;
 
-	// The color for the text we will be displaying
+    // The color for the text we will be displaying
     SDL_Color white = {255, 255, 255, 0};
 
-	// connects our texture with dest to control position
-	SDL_QueryTexture(tex, NULL, NULL, &dest.w, &dest.h);
+    // connects our texture with dest to control position
+    SDL_QueryTexture(tex, NULL, NULL, &dest.w, &dest.h);
 
-	// adjust height and width of our image box.
-	dest.w /= 6;
-	dest.h /= 6;
+    // adjust height and width of our image box.
+    dest.w /= 6;
+    dest.h /= 6;
 
-	// sets initial x-position of object
-	dest.x = (1000 - dest.w) / 2;
+    // sets initial x-position of object
+    dest.x = (1000 - dest.w) / 2;
 
-	// sets initial y-position of object
-	dest.y = (1000 - dest.h) / 2;
+    // sets initial y-position of object
+    dest.y = (1000 - dest.h) / 2;
 
-	// controls animation loop
-	int close = 0;
+    // controls animation loop
+    int close = 0;
 
-	// speed of box
-	int speed = 300;
+    // speed of box
+    int speed = 300;
 
-	// animation loop
-	while (!close) {
-		SDL_Event event;
+    // animation loop
+    while (!close) {
+        SDL_Event event;
 
-		// Events management
-		while (SDL_PollEvent(&event)) {
-			switch (event.type) {
+        // Events management
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
 
-			case SDL_QUIT:
-				// handling of close button
-				close = 1;
-				break;
+            case SDL_QUIT:
+                // handling of close button
+                close = 1;
+                break;
 
-			case SDL_KEYDOWN:
-				// keyboard API for key pressed
-				switch (event.key.keysym.scancode) {
-				case SDL_SCANCODE_W:
-				case SDL_SCANCODE_UP:
-					dest.y -= speed / 30;
-					break;
-				case SDL_SCANCODE_A:
-				case SDL_SCANCODE_LEFT:
-					dest.x -= speed / 30;
-					break;
-				case SDL_SCANCODE_S:
-				case SDL_SCANCODE_DOWN:
-					dest.y += speed / 30;
-					break;
-				case SDL_SCANCODE_D:
-				case SDL_SCANCODE_RIGHT:
-					dest.x += speed / 30;
-					break;
-				default:
-					break;
-				}
-			}
-		}
+            case SDL_KEYDOWN:
+                // keyboard API for key pressed
+                switch (event.key.keysym.scancode) {
+                case SDL_SCANCODE_W:
+                case SDL_SCANCODE_UP:
+                    dest.y -= speed / 30;
+                    break;
+                case SDL_SCANCODE_A:
+                case SDL_SCANCODE_LEFT:
+                    dest.x -= speed / 30;
+                    break;
+                case SDL_SCANCODE_S:
+                case SDL_SCANCODE_DOWN:
+                    dest.y += speed / 30;
+                    break;
+                case SDL_SCANCODE_D:
+                case SDL_SCANCODE_RIGHT:
+                    dest.x += speed / 30;
+                    break;
+                default:
+                    break;
+                }
+            }
+        }
 
-		// right boundary
-		if (dest.x + dest.w > 1000)
-			dest.x = 1000 - dest.w;
+        // right boundary
+        if (dest.x + dest.w > 1000)
+            dest.x = 1000 - dest.w;
 
-		// left boundary
-		if (dest.x < 0)
-			dest.x = 0;
+        // left boundary
+        if (dest.x < 0)
+            dest.x = 0;
 
-		// bottom boundary
-		if (dest.y + dest.h > 1000)
-			dest.y = 1000 - dest.h;
+        // bottom boundary
+        if (dest.y + dest.h > 1000)
+            dest.y = 1000 - dest.h;
 
-		// upper boundary
-		if (dest.y < 0)
-			dest.y = 0;
+        // upper boundary
+        if (dest.y < 0)
+            dest.y = 0;
 
-		// clears the screen
-		SDL_RenderClear(rend);
-		SDL_RenderCopy(rend, tex, NULL, &dest);
+        // clears the screen
+        SDL_RenderClear(rend);
+        SDL_RenderCopy(rend, tex, NULL, &dest);
 
-		// so we can have nice text
+        // so we can have nice text
         render_text(rend, 10, 10,               "Hello World!", font, &text_rect, &white);
         render_text(rend, 10, text_rect.y + text_rect.h, "Conan demo by JFrog", font, &text_rect, &white);
 
-		// triggers the double buffers
-		// for multiple rendering
-		SDL_RenderPresent(rend);
+        // triggers the double buffers
+        // for multiple rendering
+        SDL_RenderPresent(rend);
 
-		// calculates to 60 fps
-		SDL_Delay(1000 / 60);
-	}
+        // calculates to 60 fps
+        SDL_Delay(1000 / 60);
+    }
 
-	// close TTF
-	TTF_Quit();
+    // close TTF
+    TTF_Quit();
 
-	// destroy texture
-	SDL_DestroyTexture(tex);
+    // destroy texture
+    SDL_DestroyTexture(tex);
 
-	// destroy renderer
-	SDL_DestroyRenderer(rend);
+    // destroy renderer
+    SDL_DestroyRenderer(rend);
 
-	// destroy window
-	SDL_DestroyWindow(win);
-	
-	// close SDL
-	SDL_Quit();
+    // destroy window
+    SDL_DestroyWindow(win);
+    
+    // close SDL
+    SDL_Quit();
 
-	return 0;
+    return 0;
 }
