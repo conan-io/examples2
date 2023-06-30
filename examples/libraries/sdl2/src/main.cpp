f69228b07736d0541495721a8e4ec21bee2959ef
Christopher McArthur <christopherm@jfrog.com>
2023-06-30 15:11:07 -0700

    step 3

--- examples/libraries/sdl2/src/main.cpp
+++ examples/libraries/sdl2/src/main.cpp
@@ -50,17 +50,77 @@ int main(int argc, char *argv[])
     // sets initial y-position of object
     dest.y = (1000 - dest.h) / 2;
 
-    //////
-    // We'll add code here sortly ;-)
-    // Everthing Below will get moved in the next step!
-    //////
+   // controls animation loop
+    int close = 0;
 
-    // clears the screen
-    SDL_RenderClear(rend);
+    // speed of box
+    int speed = 300;
 
-    // We add a delay in order to see that our window
-    // has successfully popped up.
-    SDL_Delay(3000);
+    // animation loop
+    while (!close) {
+        SDL_Event event;
+
+        // Events management
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+
+            case SDL_QUIT:
+                // handling of close button
+                close = 1;
+                break;
+
+            case SDL_KEYDOWN:
+                // keyboard API for key pressed
+                switch (event.key.keysym.scancode) {
+                case SDL_SCANCODE_W:
+                case SDL_SCANCODE_UP:
+                    dest.y -= speed / 30;
+                    break;
+                case SDL_SCANCODE_A:
+                case SDL_SCANCODE_LEFT:
+                    dest.x -= speed / 30;
+                    break;
+                case SDL_SCANCODE_S:
+                case SDL_SCANCODE_DOWN:
+                    dest.y += speed / 30;
+                    break;
+                case SDL_SCANCODE_D:
+                case SDL_SCANCODE_RIGHT:
+                    dest.x += speed / 30;
+                    break;
+                default:
+                    break;
+                }
+            }
+        }
+
+        // right boundary
+        if (dest.x + dest.w > 1000)
+            dest.x = 1000 - dest.w;
+
+        // left boundary
+        if (dest.x < 0)
+            dest.x = 0;
+
+        // bottom boundary
+        if (dest.y + dest.h > 1000)
+            dest.y = 1000 - dest.h;
+
+        // upper boundary
+        if (dest.y < 0)
+            dest.y = 0;
+
+        // clears the screen
+        SDL_RenderClear(rend);
+        SDL_RenderCopy(rend, tex, NULL, &dest);
+
+        // triggers the double buffers
+        // for multiple rendering
+        SDL_RenderPresent(rend);
+
+        // calculates to 60 fps
+        SDL_Delay(1000 / 60);
+    }
 
     // We destroy our window. We are passing in the pointer
     // that points to the memory allocated by the 
