575ccb108b95fba535f4c0f01c1441e318d4aada
Chris McArthur <prince.chrismc@gmail.com>
2023-07-05 17:11:10 -0700

    step 1: basic window

--- examples/libraries/sdl2/src/main.c
+++ examples/libraries/sdl2/src/main.c
@@ -1,30 +1,5 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_timer.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
-
-void render_text(
-    SDL_Renderer *renderer,
-    int x,
-    int y,
-    const char *text,
-    TTF_Font *font,
-    SDL_Rect *rect,
-    SDL_Color *color
-) {
-    SDL_Surface *surface;
-    SDL_Texture *texture;
-
-    surface = TTF_RenderText_Solid(font, text, *color);
-    texture = SDL_CreateTextureFromSurface(renderer, surface);
-    rect->x = x;
-    rect->y = y;
-    rect->w = surface->w;
-    rect->h = surface->h;
-    SDL_FreeSurface(surface);
-    SDL_RenderCopy(renderer, texture, NULL, rect);
-    SDL_DestroyTexture(texture);
-}
 
 int main(int argc, char *argv[])
 {
@@ -35,14 +10,7 @@ int main(int argc, char *argv[])
     SDL_Window* win = SDL_CreateWindow("GAME", // creates a window
                                     SDL_WINDOWPOS_CENTERED,
                                     SDL_WINDOWPOS_CENTERED,
-                                    1000, 1000, SDL_WINDOW_SHOWN);
-    /* Init TTF. */
-    TTF_Init();
-    TTF_Font *font = TTF_OpenFont("Roboto-Regular.ttf", 24);
-    if (font == NULL) {
-        fprintf(stderr, "error: font not found\n");
-        exit(EXIT_FAILURE);
-    }
+                                    1000, 1000, 0);
 
     // triggers the program that controls
     // your graphics hardware and sets flags
@@ -51,131 +19,27 @@ int main(int argc, char *argv[])
     // creates a renderer to render our images
     SDL_Renderer* rend = SDL_CreateRenderer(win, -1, render_flags);
 
-    // creates a surface to load an image into the main memory
-    SDL_Surface* surface;
+    ///
+    /// Section 2: SDL image loader
+    ///
 
-    // please provide a path for your image
-    surface = IMG_Load("conan-logo.png");
+    ///
+    /// Section 4: SDL ttf and rendering text
+    ///
 
-    // loads image to our graphics hardware memory.
-    SDL_Texture* tex = SDL_CreateTextureFromSurface(rend, surface);
+    ///
+    /// Section 3: Game Loop and Basic Controlls
+    ///
 
-    // clears main-memory
-    SDL_FreeSurface(surface);
 
-    // let us control our image position
-    // so that we can move it with our keyboard.
-    SDL_Rect dest;
+    // We add a delay in order to see that our window
+    // has successfully popped up.
+    SDL_Delay(3000);
 
-    // connects our texture with dest to control position
-    SDL_QueryTexture(tex, NULL, NULL, &dest.w, &dest.h);
+    ///
+    /// Section 5: Freeing resources
+    ///
 
-    // adjust height and width of our image box.
-    dest.w /= 6;
-    dest.h /= 6;
-
-    // sets initial x-position of object
-    dest.x = (1000 - dest.w) / 2;
-
-    // sets initial y-position of object
-    dest.y = (1000 - dest.h) / 2;
-
-   // controls animation loop
-    int close = 0;
-
-    // speed of box
-    int speed = 300;
-
-    // animation loop
-    while (!close) {
-        SDL_Event event;
-
-        // Events management
-        while (SDL_PollEvent(&event)) {
-            switch (event.type) {
-
-            case SDL_QUIT:
-                // handling of close button
-                close = 1;
-                break;
-
-            case SDL_KEYDOWN:
-                // keyboard API for key pressed
-                switch (event.key.keysym.scancode) {
-                case SDL_SCANCODE_ESCAPE:
-                    close = 1;
-                    break;
-                case SDL_SCANCODE_W:
-                case SDL_SCANCODE_UP:
-                    dest.y -= speed / 30;
-                    break;
-                case SDL_SCANCODE_A:
-                case SDL_SCANCODE_LEFT:
-                    dest.x -= speed / 30;
-                    break;
-                case SDL_SCANCODE_S:
-                case SDL_SCANCODE_DOWN:
-                    dest.y += speed / 30;
-                    break;
-                case SDL_SCANCODE_D:
-                case SDL_SCANCODE_RIGHT:
-                    dest.x += speed / 30;
-                    break;
-                default:
-                    break;
-                }
-            }
-        }
-
-        // right boundary
-        if (dest.x + dest.w > 1000)
-            dest.x = 1000 - dest.w;
-
-        // left boundary
-        if (dest.x < 0)
-            dest.x = 0;
-
-        // bottom boundary
-        if (dest.y + dest.h > 1000)
-            dest.y = 1000 - dest.h;
-
-        // upper boundary
-        if (dest.y < 0)
-            dest.y = 0;
-
-        // clears the screen
-        SDL_RenderClear(rend);
-        SDL_RenderCopy(rend, tex, NULL, &dest);
-
-        // create a rectangle to update with the size of the rendered text
-        SDL_Rect text_rect;
-
-        // The color for the text we will be displaying
-        SDL_Color white = {255, 255, 255, 0};
-
-        // so we can have nice text, two lines one above the next
-        render_text(rend, 10, 10, "Hello World!", font, &text_rect, &white);
-        render_text(rend, 10, text_rect.y + text_rect.h, "Conan demo by JFrog", font, &text_rect, &white);
-
-        // triggers the double buffers
-        // for multiple rendering
-        SDL_RenderPresent(rend);
-
-        // calculates to 60 fps
-        SDL_Delay(1000 / 60);
-    }
-
-    // close font handle
-    TTF_CloseFont(font);
-
-    // close TTF
-    TTF_Quit();
-
-    // destroy texture
-    SDL_DestroyTexture(tex);
-
-    // destroy renderer
-    SDL_DestroyRenderer(rend);
 
     // We destroy our window. We are passing in the pointer
     // that points to the memory allocated by the 
