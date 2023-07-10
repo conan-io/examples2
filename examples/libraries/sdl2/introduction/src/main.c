f0861088b7795d46fe9442a466fa59544ab1cd10
Christopher McArthur <christopherm@jfrog.com>
2023-07-10 13:43:50 -0700

    step 3 basic game loop

--- examples/libraries/sdl2/introduction/src/main.c
+++ examples/libraries/sdl2/introduction/src/main.c
@@ -59,22 +59,86 @@ int main(int argc, char *argv[])
 
     ///
     /// Section 3: Game Loop and Basic Controlls
-    ///            Note: The rest of this snippet will be removed
+    ///
 
-    while (1)
-    {
+    // controls animation loop
+    int close = 0;
+
+    // speed of box
+    int speed = 300;
+
+    // animation loop
+    while (!close) {
+        SDL_Event event;
+
+        // Events management
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+
+            case SDL_QUIT:
+                // handling of close button
+                close = 1;
+                break;
+
+            case SDL_KEYDOWN:
+                // keyboard API for key pressed
+                switch (event.key.keysym.scancode) {
+                case SDL_SCANCODE_ESCAPE:
+                    close = 1;
+                    break;
+                case SDL_SCANCODE_W:
+                case SDL_SCANCODE_UP:
+                    dest.y -= speed / 30;
+                    break;
+                case SDL_SCANCODE_A:
+                case SDL_SCANCODE_LEFT:
+                    dest.x -= speed / 30;
+                    break;
+                case SDL_SCANCODE_S:
+                case SDL_SCANCODE_DOWN:
+                    dest.y += speed / 30;
+                    break;
+                case SDL_SCANCODE_D:
+                case SDL_SCANCODE_RIGHT:
+                    dest.x += speed / 30;
+                    break;
+                default:
+                    break;
+                }
+            }
+        }
+
+        // right boundary
+        if (dest.x + dest.w > 1000)
+            dest.x = 1000 - dest.w;
+
+        // left boundary
+        if (dest.x < 0)
+            dest.x = 0;
+
+        // bottom boundary
+        if (dest.y + dest.h > 1000)
+            dest.y = 1000 - dest.h;
+
+        // upper boundary
+        if (dest.y < 0)
+            dest.y = 0;
+
         // clears the screen
         SDL_RenderClear(rend);
         SDL_RenderCopy(rend, tex, NULL, &dest);
 
+       ///
+       /// Section 4: SDL ttf and rendering text
+       ///
+
         // triggers the double buffers
         // for multiple rendering
         SDL_RenderPresent(rend);
-    }
 
-    ///
-    /// Section 3: Game Loop and Basic Controlls
-    ///            Note: The code above will be removed
+        // calculates to 60 fps
+        SDL_Delay(1000 / 60);
+    }
 
     ///
     /// Section 5: Freeing resources
