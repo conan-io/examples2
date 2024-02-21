74e9f175065dde0bdc5a5d99c25d291091f9f149
memsharded <james@conan.io>
2024-02-22 00:41:59 +0100

    review

--- examples/tools/cmake/in_package_config_files/conanfile.py
+++ examples/tools/cmake/in_package_config_files/conanfile.py
@@ -40,6 +40,7 @@ class pkgRecipe(ConanFile):
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = ["pkg"]
+        # No information provided, only the in-package .cmake is used here
+        # Other build systems or CMake via CMakeDeps will fail
         self.cpp_info.builddirs = ["pkg/cmake"]
         self.cpp_info.set_property("cmake_find_mode", "none")
