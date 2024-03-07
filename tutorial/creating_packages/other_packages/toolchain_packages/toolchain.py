2b0e7b9088bd3ad40ae7d57ab76020d228d55e15
czoido <mrgalleta@gmail.com>
2024-03-07 22:37:31 +0100

    wip

--- tutorial/creating_packages/other_packages/toolchain_packages/toolchain.py
+++ tutorial/creating_packages/other_packages/toolchain_packages/toolchain.py
@@ -10,7 +10,7 @@ class ArmToolchainPackage(ConanFile):
 
     license = "GPL-3.0-only"
     homepage = "https://developer.arm.com/downloads/-/arm-gnu-toolchain-downloads"
-    description = "Conan package for the ARM toolchain, targeting Linux ARMv7hf and ARMv8."
+    description = "Conan package for the ARM toolchain, targeting different Linux ARM architectures."
     settings = "os", "arch"
     package_type = "application"
 
@@ -21,12 +21,12 @@ class ArmToolchainPackage(ConanFile):
         self.info.settings_target.rm_safe("compiler")
         self.info.settings_target.rm_safe("build_type")
 
-    def get_toolchain(self, target_arch):
+    def _get_toolchain(self, target_arch):
         if target_arch in ["armv6", "armv7", "armv7hf"]:
-            return ("x86_64-arm-none-linux-gnueabihf", 
+            return ("arm-none-linux-gnueabihf", 
                     "df0f4927a67d1fd366ff81e40bd8c385a9324fbdde60437a512d106215f257b3")
         else:
-            return ("x86_64-aarch64-none-linux-gnu", 
+            return ("aarch64-none-linux-gnu", 
                     "12fcdf13a7430655229b20438a49e8566e26551ba08759922cdaf4695b0d4e23")
 
     def validate(self):
@@ -46,27 +46,28 @@ class ArmToolchainPackage(ConanFile):
         if self.settings.arch != "x86_64" or self.settings.os != "Linux":
             raise ConanInvalidConfiguration(f"This toolchain is not compatible with {self.settings.os}-{self.settings.arch}. "
                                             "It can only run on Linux-x86_64.")
+
     def source(self):
         download(self, "https://developer.arm.com/GetEula?Id=37988a7c-c40e-4b78-9fd1-62c20b507aa8", "LICENSE", verify=False)
 
     def build(self):
-        toolchain, sha = self.get_toolchain(self.settings_target.arch)
-        get(self, f"https://developer.arm.com/-/media/Files/downloads/gnu/13.2.rel1/binrel/arm-gnu-toolchain-13.2.rel1-{toolchain}.tar.xz",
+        toolchain, sha = self._get_toolchain(self.settings_target.arch)
+        get(self, f"https://developer.arm.com/-/media/Files/downloads/gnu/13.2.rel1/binrel/arm-gnu-toolchain-13.2.rel1-x86_64-{toolchain}.tar.xz",
             sha256=sha, strip_root=True)            
 
     def package(self):
-        toolchain, _ = self.get_toolchain(self.settings_target.arch)
+        toolchain, _ = self._get_toolchain(self.settings_target.arch)
         dirs_to_copy = [toolchain, "bin", "include", "lib", "libexec"]
         for dir_name in dirs_to_copy:
             copy(self, pattern=f"{dir_name}/*", src=self.build_folder, dst=self.package_folder, keep_path=True)
         copy(self, "LICENSE", src=self.build_folder, dst=os.path.join(self.package_folder, "licenses"), keep_path=False)
 
     def package_info(self):
-        toolchain, _ = self.get_toolchain(self.settings_target.arch)
+        toolchain, _ = self._get_toolchain(self.settings_target.arch)
         self.cpp_info.bindirs.append(os.path.join(self.package_folder, toolchain, "bin"))
 
         self.conf_info.define("tools.build:compiler_executables", {
-            "c": "arm-none-linux-gnueabihf-gcc",
-            "cpp": "arm-none-linux-gnueabihf-g++",
-            "asm": "arm-none-linux-gnueabihf-as"
+            "c":   f"{toolchain}-gcc",
+            "cpp": f"{toolchain}-g++",
+            "asm": f"{toolchain}-as"
         })
