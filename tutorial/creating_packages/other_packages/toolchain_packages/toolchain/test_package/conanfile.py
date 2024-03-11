18dc2f3db14d5338b161202ec745415bd7643f82
Carlos Zoido <mrgalleta@gmail.com>
2024-03-11 08:49:24 +0100

    Update tutorial/creating_packages/other_packages/toolchain_packages/toolchain/test_package/conanfile.py

--- tutorial/creating_packages/other_packages/toolchain_packages/toolchain/test_package/conanfile.py
+++ tutorial/creating_packages/other_packages/toolchain_packages/toolchain/test_package/conanfile.py
@@ -4,7 +4,7 @@ from conan.tools.cmake import CMake, cmake_layout
 import os
 
 
-class TestPackgeConan(ConanFile):
+class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     generators = "CMakeToolchain", "VirtualBuildEnv"
 
