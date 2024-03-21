a8f9a3731cd0c16c76d63c29c2d10924336e10ae
czoido <mrgalleta@gmail.com>
2024-03-21 13:08:39 +0100

    add example

---
+++ examples/libraries/libcurl/ascii_art_color/ci_test_example.py
@@ -0,0 +1,10 @@
+import platform
+from test.examples_tools import run
+
+print("libcurl, stb and fmt example for Visual Studio extension post")
+
+# not using conanfile in case someone downloads the whole folder
+# we do not want to interfere with the one created by the extension
+run("conan install --requires=libcurl/8.6.0 --requires=stb/cci.20230920 --requires=fmt/10.2.1 --build=missing -g MSBuildDeps -g MSBuildToolchain --output-folder=conan")
+
+run(".\\x64\\Release\\asciiartgen.exe")
