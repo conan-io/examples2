21a8fb0b6187aaa97b062f140bbd4dc8f8f897bb
czoido <mrgalleta@gmail.com>
2024-03-21 15:56:54 +0100

    wip

--- examples/libraries/libcurl/ascii_art_color/ci_test_example.py
+++ examples/libraries/libcurl/ascii_art_color/ci_test_example.py
@@ -8,5 +8,5 @@ if platform.system() == "Windows":
     # not using conanfile in case someone downloads the whole folder
     # we do not want to interfere with the one created by the extension
     run("conan install --requires=libcurl/8.6.0 --requires=stb/cci.20230920 --requires=fmt/10.2.1 --build=missing -g MSBuildDeps -g MSBuildToolchain --output-folder=conan")
-    run('.\\conan\\conanvcvars.bat & MSBuild asciiartgen.sln /p:Configuration=Release /p:Platform="x64" & .\\conan\\deactivate_conanvcvars.bat')
+    run('.\\conan\\conanvcvars.bat & MSBuild asciiartgen.sln /p:Configuration=Release /p:Platform="x64"')
     run(".\\x64\\Release\\asciiartgen.exe")
