b0809960b96002053d954cec112f1fa4a4d306d2
Rubén Rincón Blanco <git@rinconblanco.es>
2024-04-04 19:03:48 +0200

    Use git helpers

--- examples/tools/scm/git/capture_scm/conanfile.py
+++ examples/tools/scm/git/capture_scm/conanfile.py
@@ -1,7 +1,6 @@
 from conan import ConanFile
 from conan.tools.cmake import CMake, cmake_layout
 from conan.tools.scm import Git
-from conan.tools.files import load, update_conandata
 
 
 class helloRecipe(ConanFile):
@@ -16,18 +15,12 @@ class helloRecipe(ConanFile):
 
     def export(self):
         git = Git(self, self.recipe_folder)
-        scm_url, scm_commit = git.get_url_and_commit()
-        self.output.info(f"Obtained URL: {scm_url} and {scm_commit}")
-        # we store the current url and commit in conandata.yml
-        update_conandata(self, {"sources": {"commit": scm_commit, "url": scm_url}})
+        git.coordinates_to_conandata()
 
     def source(self):
         # we recover the saved url and commit from conandata.yml and use them to get sources
         git = Git(self)
-        sources = self.conan_data["sources"]
-        self.output.info(f"Cloning sources from: {sources}")
-        git.clone(url=sources["url"], target=".")
-        git.checkout(commit=sources["commit"])
+        git.checkout_from_conandata_coordinates()
 
     def config_options(self):
         if self.settings.os == "Windows":
