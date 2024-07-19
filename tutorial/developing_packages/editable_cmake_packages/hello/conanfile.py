9b9397d077995af72e25dcf0c330fa115cde45a6
Paul Harris <harris.pc@gmail.com>
2024-07-19 12:01:37 +0800

    Add example of editable package with custom src folder layout
    Also gives an example where the consumer applies additional options to
    the dependency library.

---
+++ tutorial/developing_packages/editable_cmake_packages/hello/conanfile.py
@@ -0,0 +1,23 @@
+from conan import ConanFile
+from conan.tools.cmake import cmake_layout, CMake
+
+
+class HelloConan(ConanFile):
+    name = "hello"
+    version = "1.0"
+
+    settings = "os", "compiler", "build_type", "arch"
+
+    generators = "CMakeToolchain", "CMakeDeps"
+    requires = "say/1.0"
+
+    def layout(self):
+        cmake_layout(self)
+
+    def configure(self):
+        self.options["say"].something_custom = True
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
