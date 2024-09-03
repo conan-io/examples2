e03cc464455be8fd1b246ae08a26c6fdc7914d47
PerseoGI <perseog@jfrog.com>
2024-09-03 16:51:50 +0200

    Fix more tests

--- examples/conanfile/finalize/finalize_consume/ci_test_example.py
+++ examples/conanfile/finalize/finalize_consume/ci_test_example.py
@@ -5,13 +5,13 @@ print("- Finalize test dependency/consumer -")
 
 dep_output = run("conan create dependency")
 
-assert re.search("Running finalize method in (.*)(/f)", dep_output)
-assert re.search("Running package_info method in (.*)(/f)", dep_output)
+assert re.search("Running finalize method in (.*)f", dep_output)
+assert re.search("Running package_info method in (.*)f", dep_output)
 
 consumer_output = run("conan create consumer")
 
 assert "Running generate method" in consumer_output
-assert re.search("Dependency package_folder: (.*)(/f)", consumer_output)
+assert re.search("Dependency package_folder: (.*)f", consumer_output)
 
 package_folder_content = """
 consumer/1.0: Content in dependency package_folder:
@@ -19,5 +19,5 @@ consumer/1.0: Content in dependency package_folder:
 """  
 assert package_folder_content in consumer_output
 
-assert re.search("Dependency immutable_package_folder: (.*)(/p)", consumer_output)
+assert re.search("Dependency immutable_package_folder: (.*)p", consumer_output)
 
