7aed20ea1aa56c7012c53f46fa53f35a7b220e2c
PerseoGI <perseog@jfrog.com>
2024-09-03 11:17:19 +0200

    Added cache integrity check to finalize_method test

--- examples/conanfile/finalize/finalize_method/ci_test_example.py
+++ examples/conanfile/finalize/finalize_method/ci_test_example.py
@@ -5,3 +5,6 @@ print("- Finalize test -")
 
 output = run("conan create .")
 assert re.search("whoisconan/1.0: Finalized folder (.*)(/f)", output)
+
+cache_integrity = run("conan cache check-integrity whoisconan/1.0")
+assert "Integrity checked: ok" in cache_integrity
