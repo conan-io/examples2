47919a2c0d6305c3f7f4bc3e45dff9686df132d0
Abril Rincón Blanco <git@rinconblanco.es>
2024-09-04 09:04:55 +0200

    Final fix

--- examples/conanfile/finalize/finalize_method/ci_test_example.py
+++ examples/conanfile/finalize/finalize_method/ci_test_example.py
@@ -4,7 +4,7 @@ import re
 print("- Finalize test -")
 
 output = run("conan create .")
-assert re.search("whoisconan/1.0: Finalized folder (.*)(/f)", output)
+assert re.search("whoisconan/1.0: Finalized folder (.*)(f)", output)
 
 cache_integrity = run("conan cache check-integrity whoisconan/1.0")
 assert "Integrity checked: ok" in cache_integrity
