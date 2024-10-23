1a23f1f49e073830ddba58a140ebbb893037afd1
danimtb <danimanzaneque@gmail.com>
2024-10-23 17:00:31 +0200

    fix

--- examples/tools/ros/rosenv/docker_commands.sh
+++ examples/tools/ros/rosenv/docker_commands.sh
@@ -4,22 +4,22 @@ apt-get update && apt-get install -y python3 python3-pip
 python3 -m pip install conan
 
 # Setup the ROS environment
-source /opt/ros/humble/setup.bash
+. /opt/ros/humble/setup.bash
 
 # Setup Conan profile
 conan profile detect
 
 cd workspace
 # Install the Conan dependencies of Consumer's package
-#conan install consumer/conanfile.txt --build=missing --output-folder install/conan
+conan install consumer/conanfile.txt --build=missing --output-folder install/conan
 
 # Setup the environment to find Conan installed dependencies
-#source install/conan/conanrosenv.sh
+. install/conan/conanrosenv.sh
 
 # Perform the build
-#colcon build
+colcon build
 
 # Setup the run environment
-source install/setup.bash
+. install/setup.bash
 # Run the app
-# ros2 run app main
+ros2 run app main
