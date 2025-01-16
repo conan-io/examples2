8bfa55e69c53f3d17cc77a4c637cc0a3e6e17302
czoido <mrgalleta@gmail.com>
2025-01-16 14:43:24 +0100

    check out

--- tutorial/creating_packages/build_method/ci_test_example.py
+++ tutorial/creating_packages/build_method/ci_test_example.py
@@ -5,8 +5,12 @@ print("- Building and running tests in the build() method -")
 
 out = run(f"conan create . --build=missing")
 
+print(out)
+
 assert "Running 1 test from 1 test suite." in out
 
 out = run(f"conan create . --build=missing -c tools.build:skip_test=True")
+
+print(out)
 
 assert "Running 1 test from 1 test suite." not in out
