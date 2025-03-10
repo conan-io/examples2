9ac69940e1d23cd65ffad75aa52c024a77865c87
Uilian Ries <uilianries@gmail.com>
2025-03-10 09:03:04 +0100

    Skip Yocto build on Mac
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- examples/cross_build/yocto/ci_test_example.sh
+++ examples/cross_build/yocto/ci_test_example.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-set -e
+set -ex
+
+if [ "$(uname)" != "Linux" ]; then
+    echo "INFO: Skipping Yocto build on non-Linux platform."
+    exit 0
+fi
 
 yocto_release=scarthgap
 
@@ -16,18 +21,20 @@ git clone --branch conan2/${yocto_release} https://github.com/conan-io/meta-cona
 mkdir -p poky/meta-conan/recipes-example
 cp conan-mosquitto_2.0.18.bb poky/meta-conan/recipes-example/conan-mosquitto_2.0.18.bb
 
-cd poky
+cd poky/
 source oe-init-build-env
 
-echo "INFO: Adding layers"
-bitbake-layers add-layer ../meta-openembedded/meta-oe
-bitbake-layers add-layer ../meta-openembedded/meta-python
-bitbake-layers add-layer ../meta-conan
-
-echo "INFO: Present layers"
-bitbake-layers show-layers
-
 if [ -n "${CONAN_YOCTO_BUILD_MOSQUITTO}" ]; then
+
+    echo "INFO: Adding layers"
+    bitbake-layers add-layer ../meta-openembedded/meta-oe
+    bitbake-layers add-layer ../meta-openembedded/meta-python
+    bitbake-layers add-layer ../meta-conan
+
+    echo "INFO: Present layers"
+    bitbake-layers show-layers
+
+
     echo 'IMAGE_INSTALL:append = " conan-mosquitto"' >> conf/local.conf
 
     echo "INFO: Fetching mosquitto"
@@ -38,12 +45,12 @@ if [ -n "${CONAN_YOCTO_BUILD_MOSQUITTO}" ]; then
     bitbake -v -c compile conan-mosquitto
     bitbake -v -c package conan-mosquitto    
 else:
-    echo "INFO: Skipping mosquitto build due large time. Set CONAN_YOCTO_BUILD_MOSQUITTO to build mosquitto."
+    echo "INFO: Skipping mosquitto build due large time. Set the environment variable CONAN_YOCTO_BUILD_MOSQUITTO to build mosquitto."
 fi
 
 if [ -n "${CONAN_YOCTO_BUILD_IMAGE}" ]; then
     echo "INFO: Building core-image-minimal"
     bitbake -v core-image-minimal
 else:
-    echo "INFO: Skipping image build due large time. Set CONAN_YOCTO_BUILD_IMAGE to build image."
+    echo "INFO: Skipping image build due large time. Set the environment variable CONAN_YOCTO_BUILD_IMAGE to build image."
 fi
