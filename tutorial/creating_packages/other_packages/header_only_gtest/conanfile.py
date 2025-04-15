3add27518acaa020eed46199a6424ce5f95338ab
Carlos Zoido <mrgalleta@gmail.com>
2025-04-15 06:48:42 +0200

    Update tutorial/creating_packages/other_packages/header_only_gtest/conanfile.py

--- tutorial/creating_packages/other_packages/header_only_gtest/conanfile.py
+++ tutorial/creating_packages/other_packages/header_only_gtest/conanfile.py
@@ -13,7 +13,7 @@ class SumConan(ConanFile):
     no_copy_source = True
     generators = "CMakeToolchain", "CMakeDeps"
     # Important, define the package_type
-     package_type = "header-library"
+    package_type = "header-library"
 
     def validate(self):
         check_min_cppstd(self, 11)
