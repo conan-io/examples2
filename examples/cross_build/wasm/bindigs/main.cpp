b516bd24eea3a4d10a457ccc28f778d7b4e7c0ed
PerseoGI <perseog@jfrog.com>
2025-06-02 13:14:37 +0200

    Formated and linted

--- examples/cross_build/wasm/bindigs/main.cpp
+++ examples/cross_build/wasm/bindigs/main.cpp
@@ -1,7 +1,7 @@
+#include <Eigen/Core>
 #include <cstdint>
 #include <emscripten/emscripten.h>
 #include <iostream>
-#include <Eigen/Core>
 #include <string>
 
 #ifdef __cplusplus
@@ -10,45 +10,41 @@
 #define EXTERN
 #endif
 
-
-EXTERN EMSCRIPTEN_KEEPALIVE
-uint32_t fib(uint32_t n) {
-    std::cout << "Calculating Fibonacci for n = " << n << std::endl;
-    if (n <= 1) return n;
-    uint32_t a = 0, b = 1, c;
-    for (uint32_t i = 2; i <= n; ++i) {
-        c = a + b;
-        a = b;
-        b = c;
-    }
-    return c;
+EXTERN EMSCRIPTEN_KEEPALIVE uint32_t fib(uint32_t n) {
+  std::cout << "Calculating Fibonacci for n = " << n << std::endl;
+  if (n <= 1)
+    return n;
+  uint32_t a = 0, b = 1, c;
+  for (uint32_t i = 2; i <= n; ++i) {
+    c = a + b;
+    a = b;
+    b = c;
+  }
+  return c;
 }
 
-EXTERN EMSCRIPTEN_KEEPALIVE
-void printMessage(const char* message) {
-    std::cout << "Message from C: " << message << std::endl;
-    std::string script = "alert('Message from C++: " + std::string(message) + "')";
-    std::cout << "Executing script: " << script << std::endl;
-    emscripten_run_script(script.c_str());
+EXTERN EMSCRIPTEN_KEEPALIVE void printMessage(const char *message) {
+  std::cout << "Message from C: " << message << std::endl;
+  std::string script =
+      "alert('Message from C++: " + std::string(message) + "')";
+  std::cout << "Executing script: " << script << std::endl;
+  emscripten_run_script(script.c_str());
 }
 
-EXTERN EMSCRIPTEN_KEEPALIVE
-void addOne(int32_t *input, int32_t *output){
-    *output = *input + 1;
+EXTERN EMSCRIPTEN_KEEPALIVE void addOne(int32_t *input, int32_t *output) {
+  *output = *input + 1;
 }
 
-EXTERN EMSCRIPTEN_KEEPALIVE
-float sumArray(const float* data, int32_t size) {
-    // print data input 
-    std::cout << "Data input: ";
-    for (int i = 0; i < size; ++i) {
-        std::cout << data[i] << " ";
-    }
-    std::cout << std::endl;
-    Eigen::Map<const Eigen::ArrayXf> vec(data, size);
-    return vec.sum();
+EXTERN EMSCRIPTEN_KEEPALIVE float sumArray(const float *data, int32_t size) {
+  // print data input
+  std::cout << "Data input: ";
+  for (int i = 0; i < size; ++i) {
+    std::cout << data[i] << " ";
+  }
+  std::cout << std::endl;
+  Eigen::Map<const Eigen::ArrayXf> vec(data, size);
+  return vec.sum();
 }
-
 
 int main() {
   std::cout << "Hello World!" << std::endl;
