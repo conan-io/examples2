0ba12a9bea1d50546647d89bb61514e1842cc323
Carlos Zoido <mrgalleta@gmail.com>
2025-07-02 08:01:55 +0200

    fix for integrity check

--- examples/conanfile/finalize/finalize_method/ci_test_example.py
+++ examples/conanfile/finalize/finalize_method/ci_test_example.py
@@ -7,4 +7,4 @@ output = run("conan create .")
 assert re.search("whoisconan/1.0: Finalized folder (.*)(f)", output)
 
 cache_integrity = run("conan cache check-integrity whoisconan/1.0")
-assert "Integrity checked: ok" in cache_integrity
+assert "Integrity checked: ok" in cache_integrity or "Integrity check: ok" in cache_integrity
