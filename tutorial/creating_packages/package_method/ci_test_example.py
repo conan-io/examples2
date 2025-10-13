88d336832130f058d70acf027cdd8418082dc7e7
Carlos Zoido <mrgalleta@gmail.com>
2025-10-13 14:21:46 +0200

    Fix test_requires placement and gtest version (#195)
    * fix test_requires placement and gtest version
    
    * wip
    
    * wip
    
    * relaunch ci
    
    * wip
    
    * fix
    
    * wip
    
    * fix

--- tutorial/creating_packages/package_method/ci_test_example.py
+++ tutorial/creating_packages/package_method/ci_test_example.py
@@ -3,8 +3,8 @@ from test.examples_tools import run
 
 print("- Package method with cmake.install() -")
 
-out = run(f"conan create . --build=missing")
+out = run(f"conan create . --build=missing -s compiler.cppstd=17")
 
 print("- Package method with copy() -")
 
-out = run(f"conan create manual_install.py --build=missing")
+out = run(f"conan create manual_install.py -s compiler.cppstd=17 --build=missing")
