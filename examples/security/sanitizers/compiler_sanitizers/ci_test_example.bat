86a753f5cfc26075461f7b5f59a0ba46e5967792
Uilian Ries <uilianries@gmail.com>
2025-11-05 16:26:53 +0100

    Adjust batch according Windows steps
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- examples/security/sanitizers/compiler_sanitizers/ci_test_example.bat
+++ examples/security/sanitizers/compiler_sanitizers/ci_test_example.bat
@@ -7,16 +7,16 @@ copy /Y settings_user.yml "%CONAN_HOME%"
 
 echo Conan Examples 2: Compiler Sanitizers - Index Out of Bounds
 
-PUSHD index_out_of_bounds/
-conan build . -pr profiles/clang_asan -c tools.compilation:verbosity=verbose
-build/Debug/index_out_of_bounds 2>nul || echo Process completed with errors (expected for sanitizer demo)
-POPD
+CD index_out_of_bounds/
+CALL conan build . -pr ../profiles/msvc_asan -c tools.compilation:verbosity=verbose
+CALL build/Debug/index_out_of_bounds 2>nul || echo Process completed with errors (expected for sanitizer demo)
+CD ..
 
 echo Conan Examples 2: Compiler Sanitizers - Signed Integer Overflow
 
-PUSHD
-conan build . -pr profiles/clang_asan_ubsan -c tools.compilation:verbosity=verbose
-build/Debug/signed_integer_overflow 2>nul || echo Process completed with errors (expected for sanitizer demo)
-POPD
+CD signed_integer_overflow/
+CALL conan build . -pr ../profiles/msvc_asan -c tools.compilation:verbosity=verbose
+CALL build/Debug/signed_integer_overflow 2>nul || echo Process completed with errors (expected for sanitizer demo)
+CD ..
 
 exit /b 0
