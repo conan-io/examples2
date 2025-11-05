92a32b9c1671b6bc1c112cbdff81c402f04244c5
Uilian Ries <uilianr@jfrog.com>
2025-11-05 13:43:22 +0100

    Rename profile files
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- examples/security/sanitizers/compiler_sanitizers/ci_test_example.bat
+++ examples/security/sanitizers/compiler_sanitizers/ci_test_example.bat
@@ -8,14 +8,14 @@ copy /Y settings_user.yml "%CONAN_HOME%"
 echo Conan Examples 2: Compiler Sanitizers - Index Out of Bounds
 
 PUSHD index_out_of_bounds/
-conan build . -pr profiles/asan -c tools.compilation:verbosity=verbose
+conan build . -pr profiles/clang_asan -c tools.compilation:verbosity=verbose
 build/Debug/index_out_of_bounds 2>nul || echo Process completed with errors (expected for sanitizer demo)
 POPD
 
 echo Conan Examples 2: Compiler Sanitizers - Signed Integer Overflow
 
 PUSHD
-conan build . -pr profiles/asan_ubsan -c tools.compilation:verbosity=verbose
+conan build . -pr profiles/clang_asan_ubsan -c tools.compilation:verbosity=verbose
 build/Debug/signed_integer_overflow 2>nul || echo Process completed with errors (expected for sanitizer demo)
 POPD
 
