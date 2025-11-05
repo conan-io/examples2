53c5808c5479f1aaee843b0bc04b908606cc0d71
Uilian Ries <uilianr@jfrog.com>
2025-11-05 12:25:23 +0100

    Simplify example build steps
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- examples/security/sanitizers/compiler_sanitizers/ci_test_example.sh
+++ examples/security/sanitizers/compiler_sanitizers/ci_test_example.sh
@@ -8,12 +8,12 @@ cp -f settings_user.yml $(conan config home)
 
 echo "Conan Examples 2: Compiler Sanitizers - Index Out of Bounds"
 
-conan export index_out_of_bounds/
-conan build index_out_of_bounds/ --version=0.1.0 -pr profiles/asan -of index_out_of_bounds/install --build=missing -c tools.compilation:verbosity=verbose
-index_out_of_bounds/build/Debug/index_out_of_bounds || true
+cd index_out_of_bounds/
+conan build . -pr ../profiles/asan -c tools.compilation:verbosity=verbose
+build/Debug/index_out_of_bounds || true
 
 echo "Conan Examples 2: Compiler Sanitizers - Signed Integer Overflow"
 
-conan export signed_integer_overflow/
-conan build signed_integer_overflow/ --version=0.1.0 -pr profiles/asan_ubsan -of signed_integer_overflow/install --build=missing -c tools.compilation:verbosity=verbose
-signed_integer_overflow/build/Debug/signed_integer_overflow || true
+cd signed_integer_overflow/
+conan build . -pr ../profiles/asan_ubsan -c tools.compilation:verbosity=verbose
+build/Debug/signed_integer_overflow || true
