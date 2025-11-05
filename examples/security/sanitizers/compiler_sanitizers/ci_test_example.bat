e143b29ceef28317672097cbb64367d58f9cf9ae
Uilian Ries <uilianr@jfrog.com>
2025-11-05 13:07:44 +0100

    Fix sanitizers build steps
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- examples/security/sanitizers/compiler_sanitizers/ci_test_example.bat
+++ examples/security/sanitizers/compiler_sanitizers/ci_test_example.bat
@@ -7,14 +7,16 @@ copy /Y settings_user.yml "%CONAN_HOME%"
 
 echo Conan Examples 2: Compiler Sanitizers - Index Out of Bounds
 
-conan export index_out_of_bounds/
-conan build index_out_of_bounds/ --version=0.1.0 -pr profiles/asan -of index_out_of_bounds/install --build=missing -c tools.compilation:verbosity=verbose
-index_out_of_bounds/build/Debug/index_out_of_bounds 2>nul || echo Process completed with errors (expected for sanitizer demo)
+PUSHD index_out_of_bounds/
+conan build . -pr profiles/asan -c tools.compilation:verbosity=verbose
+build/Debug/index_out_of_bounds 2>nul || echo Process completed with errors (expected for sanitizer demo)
+POPD
 
 echo Conan Examples 2: Compiler Sanitizers - Signed Integer Overflow
 
-conan export signed_integer_overflow/
-conan build signed_integer_overflow/ --version=0.1.0 -pr profiles/asan_ubsan -of signed_integer_overflow/install --build=missing -c tools.compilation:verbosity=verbose
-signed_integer_overflow/build/Debug/signed_integer_overflow 2>nul || echo Process completed with errors (expected for sanitizer demo)
+PUSHD
+conan build . -pr profiles/asan_ubsan -c tools.compilation:verbosity=verbose
+build/Debug/signed_integer_overflow 2>nul || echo Process completed with errors (expected for sanitizer demo)
+POPD
 
 exit /b 0
