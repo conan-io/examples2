4c520b56477efea2a255443027eedf828f63a202
Uilian Ries <uilianr@jfrog.com>
2025-11-07 11:29:41 +0100

    No CMAKE_POLICY_DEFAULT_CMP0149
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- examples/libraries/sdl2/validation/ci_test_example.py
+++ examples/libraries/sdl2/validation/ci_test_example.py
@@ -4,4 +4,4 @@ from test.examples_tools import run
 print("Install xz_utils and validate SDK version")
 
 if platform.system() == "Windows":
-    run("conan install --requires=xz_utils/5.8.1 --build=xz_utils/5.8.1 -c:a tools.cmake.cmaketoolchain:extra_variables=\"{'CMAKE_POLICY_DEFAULT_CMP0149': 'NEW'}\"")
+    run("conan install --requires=xz_utils/5.8.1 --build=xz_utils/5.8.1")
