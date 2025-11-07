cbcdfa70f73f190b304b956352a3ad6c138c07a7
Uilian Ries <uilianr@jfrog.com>
2025-11-07 13:59:37 +0100

    Regular build
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- examples/libraries/sdl2/validation/ci_test_example.py
+++
@@ -1,7 +0,0 @@
-import platform
-from test.examples_tools import run
-
-print("Install xz_utils and validate SDK version")
-
-if platform.system() == "Windows":
-    run("conan install --requires=xz_utils/5.8.1 --build=xz_utils/5.8.1")
