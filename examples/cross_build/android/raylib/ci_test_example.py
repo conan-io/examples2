0727e971880383df3166e1a397eb617560c9e8ea
Uilian Ries <uilianr@jfrog.com>
2025-11-26 13:59:08 +0100

    Remove extra projects
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

---
+++ examples/cross_build/android/raylib/ci_test_example.py
@@ -0,0 +1,32 @@
+import os
+
+from test.examples_tools import run
+
+# ############# Example ################
+print("- Use the Android NDK to cross-build a package -")
+
+
+profile = """
+
+[settings]
+os=Android
+os.api_level=27
+arch=armv8
+compiler=clang
+compiler.version=18
+compiler.libcxx=c++_static
+compiler.cppstd=17
+build_type=Debug
+
+[conf]
+tools.android:ndk_path={}
+"""
+
+ndk_path = os.environ.get("ANDROID_NDK") or os.environ.get("ANDROID_NDK_HOME")
+
+if ndk_path:
+    profile = profile.format(ndk_path)
+    with open("android", "w") as _f:
+        _f.write(profile)
+
+    run("gradlew assembleDebug")
