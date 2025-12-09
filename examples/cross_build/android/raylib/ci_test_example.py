b2122d49d8d24be5731160dc4045e50e761c7a24
Uilian Ries <uilianr@jfrog.com>
2025-12-09 16:38:34 +0100

    Use Jinja to populate ANDROID_NDK
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- examples/cross_build/android/raylib/ci_test_example.py
+++ examples/cross_build/android/raylib/ci_test_example.py
@@ -6,28 +6,7 @@ from test.examples_tools import run
 print("- Use the Android NDK to cross-build a package -")
 
 
-profile = """
-
-[settings]
-os=Android
-os.api_level=27
-arch=armv8
-compiler=clang
-compiler.version=18
-compiler.libcxx=c++_static
-compiler.cppstd=17
-build_type=Debug
-
-[conf]
-tools.android:ndk_path={}
-"""
-
 ndk_path = os.environ.get("ANDROID_NDK") or os.environ.get("ANDROID_NDK_HOME")
 if ndk_path:
-    profile = profile.format(ndk_path)
-    os.makedirs(os.path.join("app", "build"), exist_ok=True)
-    with open(os.path.join("app", "build", "android"), "w") as fd:
-        fd.write(profile)
-
     run("gradle --no-daemon assembleDebug")
     assert os.path.exists(os.path.join("app", "build", "outputs", "apk", "debug", "app-debug.apk"))
