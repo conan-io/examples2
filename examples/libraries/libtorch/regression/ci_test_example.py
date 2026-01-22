220c93a3646b08397f8085cf11f8730849759b53
Abril Rincón Blanco <abrilrb@jfrog.com>
2026-01-22 12:11:23 +0100

    Testing

--- examples/libraries/libtorch/regression/ci_test_example.py
+++ examples/libraries/libtorch/regression/ci_test_example.py
@@ -5,21 +5,22 @@ from test.examples_tools import run, chdir
 
 print("Libtorch regression example with Conan")
 
-
-run("git clone https://github.com/pytorch/examples.git")
+run("git clone --depth 1 https://github.com/pytorch/examples.git")
 shutil.copy("conanfile.txt", "examples/cpp/regression/conanfile.txt")
 
 with chdir("examples/cpp/regression"):
-    run("conan install -b=missing -s compiler.cppstd=17")
-
-    # with presets
-
-    if platform.system() == "Windows":
-        run("cmake --preset conan-default")
-        run("cmake --build --preset conan-release")
-        run("build/Release/regression.exe")
-    else:
-        run("cmake --preset conan-release")
-        run("cmake --build --preset conan-release")
-        run("./build/Release/regression")
+    run("conan graph explain -s compiler.cppstd=17")
+    # run("conan install -b=missing -s compiler.cppstd=17")
+    #
+    # if platform.system() == "Windows":
+    #     run("cmake --preset conan-default")
+    #     run("cmake --build --preset conan-release")
+    # else:
+    #     run("cmake --preset conan-release")
+    #     run("cmake --build --preset conan-release")
+    #
+    # # Only execute this in macos in the CI, there are some issues with the generated binary and the
+    # # GitHub images
+    # if platform.system() == "Darwin":
+    #     run("./build/Release/regression")
 
