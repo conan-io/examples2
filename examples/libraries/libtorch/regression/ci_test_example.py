a04c2146cd7c309aabd1845c4883f6e28049e1d6
Abril Rincón Blanco <abrilrb@jfrog.com>
2026-01-22 12:40:07 +0100

    It's gnu...

--- examples/libraries/libtorch/regression/ci_test_example.py
+++ examples/libraries/libtorch/regression/ci_test_example.py
@@ -9,18 +9,17 @@ run("git clone --depth 1 https://github.com/pytorch/examples.git")
 shutil.copy("conanfile.txt", "examples/cpp/regression/conanfile.txt")
 
 with chdir("examples/cpp/regression"):
-    run("conan graph explain -s compiler.cppstd=17")
-    # run("conan install -b=missing -s compiler.cppstd=17")
-    #
-    # if platform.system() == "Windows":
-    #     run("cmake --preset conan-default")
-    #     run("cmake --build --preset conan-release")
-    # else:
-    #     run("cmake --preset conan-release")
-    #     run("cmake --build --preset conan-release")
-    #
-    # # Only execute this in macos in the CI, there are some issues with the generated binary and the
-    # # GitHub images
-    # if platform.system() == "Darwin":
-    #     run("./build/Release/regression")
+    run("conan install -b=missing -s compiler.cppstd=gnu17")
+
+    if platform.system() == "Windows":
+        run("cmake --preset conan-default")
+        run("cmake --build --preset conan-release")
+    else:
+        run("cmake --preset conan-release")
+        run("cmake --build --preset conan-release")
+
+    # Only execute this in macos in the CI, there are some issues with the generated binary and the
+    # GitHub images
+    if platform.system() == "Darwin":
+        run("./build/Release/regression")
 
