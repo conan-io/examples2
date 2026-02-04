f0df224ccab258978c86e2ca166097522faf378b
danimtb <danimanzaneque@gmail.com>
2026-02-04 18:08:05 +0100

    fix

--- examples/extensions/plugins/openssl_sign/ci_test_example.py
+++ examples/extensions/plugins/openssl_sign/ci_test_example.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 from test.examples_tools import run
 
@@ -22,4 +23,4 @@ assert "Package verified for reference hello/1.0" in output
 assert "[Package sign] Summary: OK=2, FAILED=0" in output
 
 conan_home = run("conan config home").strip()
-os.rmdir(os.path.join(conan_home, "extensions", "plugins", "sign"))
+shutil.rmtree(os.path.join(conan_home, "extensions", "plugins", "sign"))
