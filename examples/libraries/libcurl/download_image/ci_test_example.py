3b85594a78961e6cd71691f63f212978b19a78fc
Daniel <danimanzaneque@gmail.com>
2026-02-06 10:59:07 +0100

    Apply suggestion from @danimtb

--- examples/libraries/libcurl/download_image/ci_test_example.py
+++ examples/libraries/libcurl/download_image/ci_test_example.py
@@ -3,7 +3,7 @@ from test.examples_tools import run
 
 print("libcurl and stb example")
 
-# not using a conanfile because that will be created by the CLion plugin, in case someone just wants to
+# not using a conanfile because that will be created by the CLion plugin, in case someone just wants to 
 # copy this code to its folder so that the user does not find any conflicting file
 run("conan install --requires=stb/cci.20240531 --build=missing -g CMakeDeps -g CMakeToolchain --output-folder=build")
 run("conan install --requires=libcurl/8.12.1 --build=missing -g CMakeDeps -g CMakeToolchain --output-folder=build")
