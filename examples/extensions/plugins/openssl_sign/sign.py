fe98df4f60394cf808435b242f2a486db7e110ca
Daniel <danimanzaneque@gmail.com>
2026-02-10 17:46:05 +0100

    Update examples/extensions/plugins/openssl_sign/sign.py
    Co-authored-by: Carlos Zoido <mrgalleta@gmail.com>

--- examples/extensions/plugins/openssl_sign/sign.py
+++ examples/extensions/plugins/openssl_sign/sign.py
@@ -12,7 +12,7 @@ And extract the public key:
 
     $ openssl pkey -in private_key.pem -pubout -out public_key.pem
 
-The private_key.pem and public_key.pem files should be placed inside a folder named withe the provider's name
+The private_key.pem and public_key.pem files should be placed inside a folder named with the the provider's name
 ('my-organization' for this example). The 'my-organization' folder should be next to this plugins's file sign.py
 (inside the CONAN_HOME/extensions/plugins/sing folder).
 """
