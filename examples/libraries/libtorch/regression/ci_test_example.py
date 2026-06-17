b960f57b28b64a4ab4740b489b3eb6f5237cf8f0
Carlos Zoido <mrgalleta@gmail.com>
2026-06-17 11:43:06 +0200

    wip

--- examples/libraries/libtorch/regression/ci_test_example.py
+++ examples/libraries/libtorch/regression/ci_test_example.py
@@ -1,22 +1,24 @@
 import platform
 import shutil
+import sys
 
 from test.examples_tools import run, chdir
 
 print("Libtorch regression example with Conan")
+
+if platform.system() == "Windows":
+    print("Skipping on Windows: libtorch dependencies not compatible with MSVC 195")
+    sys.exit(0)
 
 run("git clone --depth 1 https://github.com/pytorch/examples.git")
 shutil.copy("conanfile.txt", "examples/cpp/regression/conanfile.txt")
 
 with chdir("examples/cpp/regression"):
     cppstd = "17" if platform.system() == "Windows" else "gnu17"
-    ninja_conf = " -c tools.cmake.cmaketoolchain:generator=Ninja" if platform.system() == "Windows" else ""
-    run(f"conan install -b=missing -s:a compiler.cppstd={cppstd}{ninja_conf} --update")
+    run(f"conan install -b=missing -s:a compiler.cppstd={cppstd} --update")
 
     run("cmake --preset conan-release")
-    if platform.system() != "Windows":
-        # Don't build on Windows. CI's msvc can't build this
-        run("cmake --build --preset conan-release")
+    run("cmake --build --preset conan-release")
 
     # Only execute this in macos in the CI, there are some issues with the generated binary and the
     # GitHub images
