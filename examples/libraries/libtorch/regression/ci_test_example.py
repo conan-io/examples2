8f2351e661135a7aab50b2100bf180c2394c776a
Carlos Zoido <mrgalleta@gmail.com>
2026-06-17 10:23:38 +0200

    wip

--- examples/libraries/libtorch/regression/ci_test_example.py
+++ examples/libraries/libtorch/regression/ci_test_example.py
@@ -10,7 +10,8 @@ shutil.copy("conanfile.txt", "examples/cpp/regression/conanfile.txt")
 
 with chdir("examples/cpp/regression"):
     cppstd = "17" if platform.system() == "Windows" else "gnu17"
-    run(f"conan install -b=missing -s:a compiler.cppstd={cppstd} --update")
+    ninja_conf = " -c tools.cmake.cmaketoolchain:generator=Ninja" if platform.system() == "Windows" else ""
+    run(f"conan install -b=missing -s:a compiler.cppstd={cppstd}{ninja_conf} --update")
 
     run("cmake --preset conan-release")
     if platform.system() != "Windows":
